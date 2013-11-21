6cf34efb8d384c21a4d842799867f8426b82804d
David Thompson <david.thompson@kitware.com>
2013-11-21 13:45:37 -0500

    A benchmark for SMTK model storage.
    It is now simple to swap out `google::sparse_hash_map` and `std::map`
    in `smtk::model::Storage`. This test program provides a way to assess
    the performance differences between the two.

--- smtk/model/testing/benchmarkModel.cxx
+++ smtk/model/testing/benchmarkModel.cxx
@@ -1,8 +1,12 @@
 #include "smtk/model/Storage.h"
+#include "smtk/model/ExportJSON.h"
 #include "smtk/model/ImportJSON.h"
 #include "smtk/model/testing/helpers.h"
 
 #include "cJSON.h"
+
+#include <iostream>
+#include <fstream>
 
 #include <assert.h>
 
@@ -14,50 +18,82 @@ int main(int argc, char* argv[])
 {
   (void)argc;
   (void)argv;
-  UUIDsToEntities smTopology;
-  UUIDsToArrangements smArrangements;
-  UUIDsToTessellations smTessellation;
-  Storage sm(&smTopology, &smArrangements, &smTessellation);
 
+  StoragePtr sm = Storage::New();
   Timer t;
   double deltaT;
-  int numObj = 100;
+
+  // ### Benchmark entity creation ###
+  // This includes generating new UUIDs which takes up the bulk of the time.
+  int numObj = 1000;
   t.mark();
   for (int i = 0; i < numObj; ++i)
     {
-    createTet(sm);
+    createTet(*sm.get());
     }
   deltaT = t.elapsed();
-  std::cout << numObj << " objects " << deltaT << " seconds " << (numObj / deltaT) << " objs/sec\n";
+  std::cout
+    << numObj << " objects " << deltaT << " seconds "
+    << (numObj / deltaT) << " objs/sec\n"
+    << "  " << sm->topology().size() << " entities " << deltaT << " seconds "
+    << (sm->topology().size() / deltaT) << " entities/sec\n";
 
-  int numMisses = 10000;
+  // ### Benchmark entity lookup ###
+  // #### Misses
+  int numMisses = 2000000;
   t.mark();
+  UUID nil;
   for (int i = 0; i < numMisses; ++i)
     {
-    UUID nil;
-    Entity* ent = sm.findEntity(nil);
+    Entity* ent = sm->findEntity(nil);
     (void) ent;
+    (*nil.begin())++; // twiddling bits should still result in a missing UUID.
     }
   deltaT = t.elapsed();
-  std::cout << numMisses << " missed lookups " << deltaT << " seconds " << (numMisses / deltaT) << " missed lookups/sec\n";
+  std::cout
+    << numMisses << " missed lookups " << deltaT << " seconds "
+    << (numMisses / deltaT) << " missed lookups/sec\n";
 
+  // #### Hits
   UUIDWithEntity it;
-  it = sm.topology().begin();
+  it = sm->topology().begin();
   do
     ++it;
-  while (it != sm.topology().end() && it->second.relations().empty());
-  int numHits = 10000;
+  while (it != sm->topology().end() && it->second.relations().empty());
+  int numHits = 2000000;
   t.mark();
   for (int i = 0; i < numHits; ++i)
     {
-    Entity* ent = sm.findEntity(it->second.relations().front());
+    Entity* ent = sm->findEntity(it->second.relations().front());
     do
       ++it;
-    while (it != sm.topology().end() && it->second.relations().empty());
-    if (it == sm.topology().end()) it = sm.topology().begin();
+    while (it != sm->topology().end() && it->second.relations().empty());
+    if (it == sm->topology().end()) it = sm->topology().begin();
     }
   deltaT = t.elapsed();
-  std::cout << numHits << " missed lookups " << deltaT << " seconds " << (numHits / deltaT) << " good lookups/sec\n";
+  std::cout
+    << numHits << " missed lookups " << deltaT << " seconds "
+    << (numHits / deltaT) << " good lookups/sec\n";
+
+  // ### Benchmark JSON export ###
+  t.mark();
+  std::string json = ExportJSON::fromModel(sm);
+  double jsonTime = t.elapsed();
+  t.mark();
+  std::ofstream jsonFile("/tmp/benchmark.json");
+  jsonFile << json;
+  jsonFile.close();
+  deltaT = t.elapsed();
+  std::cout << jsonTime << " seconds to generate JSON, " << deltaT << " seconds to write\n";
+
+  // ### Benchmark JSON import ###
+    {
+    StoragePtr sm2 = Storage::New();
+    t.mark();
+    ImportJSON::intoModel(json.c_str(), sm2);
+    deltaT = t.elapsed();
+    }
+  std::cout << deltaT << " seconds to ingest JSON\n";
 
   return 0;
 }
