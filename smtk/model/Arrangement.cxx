6bbcda0783152ea7e1f0012ff3c8564319641b1e
David Thompson <david.thompson@kitware.com>
2013-12-16 11:42:51 -0500

    Move enumerants to their own header/implementation.

--- smtk/model/Arrangement.cxx
+++ smtk/model/Arrangement.cxx
@@ -3,60 +3,6 @@
 namespace smtk {
  namespace model {
 
-const char* ArrangementKindName[KINDS_OF_ARRANGEMENTS + 1] = {
-  "inclusion",   // INCLUSION
-  "cell",        // CELL
-  "shell",       // SHELL
-  "use",         // USE
-  "sense",       // SENSE
-  "embedding",   // EMBEDDING
-  "invalid"      // KINDS_OF_ARRANGEMENTS
-};
-
-const char* ArrangementKindAbbr[KINDS_OF_ARRANGEMENTS + 1] = {
-  "i", // INCLUSION
-  "c", // CELL
-  "s", // SHELL
-  "u", // ORIENTATION
-  "n", // SENSE
-  "e", // EMBEDDING
-  "?"  // KINDS_OF_ARRANGEMENTS
-};
-
-ArrangementKind ArrangementKindFromName(const std::string& name)
-{
-  for (int i = 0; i < KINDS_OF_ARRANGEMENTS; ++i)
-    {
-    if (name == ArrangementKindName[i])
-      {
-      return ArrangementKind(i);
-      }
-    }
-  return KINDS_OF_ARRANGEMENTS;
-}
-
-std::string NameForArrangementKind(ArrangementKind k)
-{
-  return ArrangementKindName[k];
-}
-
-ArrangementKind ArrangementKindFromAbbreviation(const std::string& abbr)
-{
-  for (int i = 0; i < KINDS_OF_ARRANGEMENTS; ++i)
-    {
-    if (abbr == ArrangementKindAbbr[i])
-      {
-      return ArrangementKind(i);
-      }
-    }
-  return KINDS_OF_ARRANGEMENTS;
-}
-
-std::string AbbreviationForArrangementKind(ArrangementKind k)
-{
-  return ArrangementKindAbbr[k];
-}
-
 /** @name Methods to construct arrangement records.
   *
   * These should be preferred to manual construction of arrangements so that
