c8beff09f34e00daf303cc073b1ee442faa04693
John Tourtellott <john.tourtellott@kitware.com>
2013-12-18 13:39:45 -0500

    Fix compiler warning, caused by Qt implicit behavior
    Need to instantiate QLayout, but don't assign to apparently-unused var

--- smtk/Qt/qtGroupItem.cxx
+++ smtk/Qt/qtGroupItem.cxx
@@ -75,7 +75,10 @@ void qtGroupItem::createWidget()
   //   groupBox->setCheckable(true);
   //   groupBox->setChecked(true);
   this->Widget = groupBox;
-  QVBoxLayout* layout = new QVBoxLayout(this->Widget);
+  // Instantiate a layout for the widget, but do *not* assign to a variable,
+  // because that would cause a compiler warning, since the layour is not
+  // explicitly referenced anywhere else.
+  new QVBoxLayout(this->Widget);
   if(this->parentWidget())
     {
     this->parentWidget()->layout()->setAlignment(Qt::AlignTop);
