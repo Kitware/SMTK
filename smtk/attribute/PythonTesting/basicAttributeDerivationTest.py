39051b906c6d94a7b48b8c694534bafd74c53c1d
Jacob Becker <jacob.becker@kitware.com>
2014-01-01 12:52:34 -0500

    update the python test

--- smtk/attribute/PythonTesting/basicAttributeDerivationTest.py
+++ smtk/attribute/PythonTesting/basicAttributeDerivationTest.py
@@ -7,6 +7,9 @@ Requires SMTKCorePython.so to be in module path
 
 import smtk
 
+itemNames = ["IntComp1", "IntComp2", "DoubleComp1",
+             "DoubleComp2", "StringComp1", "StringComp2"]
+
 if __name__ == '__main__':
     import sys
 
@@ -17,33 +20,59 @@ if __name__ == '__main__':
     base = manager.createDefinition("BaseDef");
 
     #Lets add some item definitions
-    icompdef = smtk.attribute.IntItemDefinition.New('IntComp1')
+    icompdef = smtk.attribute.IntItemDefinition.New(itemNames[0])
     itemdef = smtk.attribute.IntItemDefinition.ToItemDefinition(icompdef)
     base.addItemDefinition(itemdef);
-    icompdef2 = smtk.attribute.IntItemDefinition.New('IntComp2')
+    icompdef2 = smtk.attribute.IntItemDefinition.New(itemNames[1])
     icompdef2.setDefaultValue(10);
     itemdef2 = smtk.attribute.IntItemDefinition.ToItemDefinition(icompdef2)
     base.addItemDefinition(itemdef2);
 
     def1 = manager.createDefinition('Derived1', 'BaseDef')
     # Lets add some item definitions
-    dcompdef = smtk.attribute.DoubleItemDefinition.New('DoubleComp1')
+    dcompdef = smtk.attribute.DoubleItemDefinition.New(itemNames[2])
     itemdef = smtk.attribute.DoubleItemDefinition.ToItemDefinition(dcompdef)
     def1.addItemDefinition(itemdef)
-    dcompdef2 = smtk.attribute.DoubleItemDefinition.New('DoubleComp2')
+    dcompdef2 = smtk.attribute.DoubleItemDefinition.New(itemNames[3])
     dcompdef2.setDefaultValue(-35.2)
     itemdef2 = smtk.attribute.DoubleItemDefinition.ToItemDefinition(dcompdef2)
     def1.addItemDefinition(itemdef2)
 
     def2 = manager.createDefinition('Derived2', 'Derived1')
     # Lets add some item definitions
-    scompdef = smtk.attribute.StringItemDefinition.New('StringComp1')
+    scompdef = smtk.attribute.StringItemDefinition.New(itemNames[4])
     itemdef = smtk.attribute.StringItemDefinition.ToItemDefinition(scompdef)
     def1.addItemDefinition(itemdef)
-    scompdef2 = smtk.attribute.StringItemDefinition.New('StringComp2')
+    scompdef2 = smtk.attribute.StringItemDefinition.New(itemNames[5])
     scompdef2.setDefaultValue('Default')
     itemdef2 = smtk.attribute.StringItemDefinition.ToItemDefinition(scompdef2)
     def1.addItemDefinition(itemdef2)
+
+    # Lets test out the find item position method
+    pstatus = 0;
+    for j in range(0,6):
+      if (def2.findItemPosition(itemNames[j]) != j):
+        print "Incorrect Position Returned for %s, position returned is %d, but it should be %d\n" % (itemNames[j], def2.findItemPosition(itemNames[j]), j)
+        pstatus = -1
+    if pstatus == 0:
+      print "Initial Position Test Passed!\n"
+    else:
+      print "Initial Position Test Failed!\n"
+      status = -1
+
+    # Lets add a  component to the base def and verify that positions are reordered
+    base.addItemDefinitionStr(smtk.attribute.StringItemDefinition, "InsertStringItem")
+    pstatus = 0
+    for j in range(2,6):
+      if def2.findItemPosition(itemNames[j]) != (j+1):
+        print "Incorrect Position Returned for %s, position returned is %d, but it should be %d\n" % (itemNames[j], def2.findItemPosition(itemNames[j]), j+1)
+        pstatus = -1;
+
+    if pstatus == 0:
+      print "Insertion Position Test Passed!\n"
+    else:
+      print "Insertion Position Test Failed!\n"
+      status = -1
 
     att = manager.createAttribute('testAtt', 'Derived2')
     if not att is None:
