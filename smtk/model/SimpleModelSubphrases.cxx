e89f1b227cac42d362a18f482e7478f9367189ec
David Thompson <david.thompson@kitware.com>
2014-01-29 16:43:02 -0500

    Prettify subphrases; sort them.
    This does destroy some information that can be useful,
    such as the ordering of edges within a face. But it
    makes for much easier-to-digest presentations of group
    members.
    
    Also, we sort by descriptive phrase type, entity type,
    entity dimension, and finally title (with special
    attention paid to numeric differences) to preserve
    groupings.

--- smtk/model/SimpleModelSubphrases.cxx
+++ smtk/model/SimpleModelSubphrases.cxx
@@ -10,8 +10,111 @@
 #include "smtk/model/ShellEntity.h"
 #include "smtk/model/UseEntity.h"
 
+#include <algorithm>
+
+#include <ctype.h> // for isdigit
+#include <stdlib.h> // for atof
+
 namespace smtk {
   namespace model {
+
+static bool SpecialEntityNameSort(const DescriptivePhrasePtr& a, const DescriptivePhrasePtr& b)
+{
+  static const int sortOrder[] = {
+     1, // ENTITY_LIST
+     0, // ENTITY_SUMMARY
+     2, // ARRANGEMENT_LIST
+     3, // ATTRIBUTE_LIST
+     4, // MODEL_INCLUDES_ENTITY
+     5, // MODEL_EMBEDDED_IN_MODEL
+     6, // CELL_INCLUDES_CELL
+     7, // CELL_EMBEDDED_IN_CELL
+     8, // CELL_HAS_SHELL
+     9, // CELL_HAS_USE
+    10, // SHELL_HAS_CELL
+    11, // SHELL_HAS_USE
+    12, // USE_HAS_CELL
+    13, // USE_HAS_SHELL
+    15, // FLOAT_PROPERTY_LIST
+    16, // STRING_PROPERTY_LIST
+    14, // INTEGER_PROPERTY_LIST
+    18, // ENTITY_HAS_FLOAT_PROPERTY
+    19, // ENTITY_HAS_STRING_PROPERTY
+    17, // ENTITY_HAS_INTEGER_PROPERTY
+    20, // ENTITY_HAS_ATTRIBUTE
+    22, // FLOAT_PROPERTY_VALUE
+    23, // STRING_PROPERTY_VALUE
+    21, // INTEGER_PROPERTY_VALUE
+    24, // ENTITY_HAS_SUBPHRASES
+    25  // INVALID_DESCRIPTION
+  };
+
+  // I. Sort by phrase type.
+  DescriptivePhraseType pta = a->phraseType();
+  DescriptivePhraseType ptb = b->phraseType();
+  if (pta != ptb)
+    return sortOrder[pta] < sortOrder[ptb];
+
+  // II. Sort by entity type/dimension
+  // II.a. Entity type
+  BitFlags eta = a->relatedEntity().entityFlags() & ENTITY_MASK;
+  BitFlags etb = b->relatedEntity().entityFlags() & ENTITY_MASK;
+  if (eta != etb)
+    {
+    switch (eta)
+      {
+    case CELL_ENTITY:       // 0x0100
+      return etb == MODEL_ENTITY ? false : true;
+    case USE_ENTITY:        // 0x0200
+      return etb == MODEL_ENTITY || etb < USE_ENTITY ? false : true;
+    case SHELL_ENTITY:      // 0x0400
+      return etb == MODEL_ENTITY || etb < SHELL_ENTITY ? false : true;
+    case GROUP_ENTITY:      // 0x0800
+      return etb == MODEL_ENTITY || etb < SHELL_ENTITY ? false : true;
+    case MODEL_ENTITY:      // 0x1000
+      return true;
+    case INSTANCE_ENTITY:   // 0x2000
+      return false;
+    default:
+      return eta < etb ? true : false;
+      }
+    }
+
+  // II.b. Dimension
+  eta = a->relatedEntity().entityFlags() & ANY_DIMENSION;
+  etb = b->relatedEntity().entityFlags() & ANY_DIMENSION;
+  if (eta != etb)
+    return eta < etb;
+
+  // III. Sort by title, with care taken when differences are numeric values.
+  std::string ta(a->title());
+  std::string tb(b->title());
+  if (ta.empty())
+    return true;
+  if (tb.empty())
+    return false;
+  std::string::size_type minlen = ta.size() < tb.size() ? ta.size() : tb.size();
+  std::string::size_type i;
+  for (i = 0; i < minlen; ++i)
+    if (ta[i] != tb[i])
+      break; // Stop at the first difference between ta and tb.
+
+  // Shorter strings are less than longer versions with the same start:
+  if (i == minlen)
+    return ta.size() < tb.size() ? true : false;
+
+  // Both ta & tb have some character present and different.
+  bool da = isdigit(ta[i]);
+  bool db = isdigit(tb[i]);
+  if ( da && !db) return true; // digits come before other things
+  if (!da &&  db) return false; // non-digits come after digits
+  if (!da && !db) return ta[i] < tb[i];
+  // Now, both ta and tb differ with some numeric value.
+  // Convert to a number and compare the numbers.
+  double na = atof(ta.substr(i).c_str());
+  double nb = atof(tb.substr(i).c_str());
+  return na < nb;
+}
 
 SimpleModelSubphrases::SimpleModelSubphrases()
   : m_passOverUses(true)
@@ -40,6 +143,10 @@ DescriptivePhrases SimpleModelSubphrases::subphrases(
   default:
     break;
     }
+
+  // Now sort the list
+  std::sort(result.begin(), result.end(), SpecialEntityNameSort);
+
   return result;
 }
 
--- smtk/model/testing/unitDescriptivePhrase.cxx
+++ smtk/model/testing/unitDescriptivePhrase.cxx
@@ -11,15 +11,22 @@
 #include <iostream>
 #include <string>
 
-#include <assert.h>
+#include <stdlib.h>
 
 using smtk::shared_ptr;
 using namespace smtk::util;
 using namespace smtk::model;
 using namespace smtk::model::testing;
 
+static int maxIndent = 10;
+
 void prindent(std::ostream& os, int indent, DescriptivePhrase::Ptr p)
 {
+  // Do not descend too far, as infinite recursion is possible,
+  // even with the SimpleSubphraseGenerator
+  if (indent > maxIndent)
+    return;
+
   os << std::string(indent, ' ') << p->title() << "  (" << p->subtitle() << ")";
   FloatList rgba = p->relatedColor();
   if (rgba[3] >= 0.)
@@ -36,6 +43,8 @@ void prindent(std::ostream& os, int indent, DescriptivePhrase::Ptr p)
 int main(int argc, char* argv[])
 {
   StoragePtr sm = Storage::create();
+  if (argc > 2)
+    maxIndent = atol(argv[2]);
 
   // Block to ensure timely destruction of JSON data.
     {
