e747328ca3aa0a501c8fb852b61c02a250fd53bd
Yumin Yuan <yumin.yuan@kitware.com>
2014-02-11 11:42:38 -0500

    cleaned up some debug code

--- smtk/Qt/qtModelView.cxx
+++ smtk/Qt/qtModelView.cxx
@@ -46,10 +46,7 @@ void qtModelView::selectionChanged (
   foreach(QModelIndex sel, this->selectedIndexes())
     {
     DescriptivePhrase* dPhrase = qmodel->getItem(sel);
-//    std::cerr << "sel row " << sel.row() << "sel col " << sel.column() << "\n";
     ids.append(dPhrase->relatedEntityId());
-    std::cout << "sel uuid " << dPhrase->relatedEntityId().toString() << " title "
-      << dPhrase->title() << "\n";
     }
 
   emit this->entitiesSelected(ids);
@@ -63,34 +60,23 @@ void qtModelView::selectEntities(const QList<std::string>& selIds)
   QEntityItemModel* qmodel = this->getModel();
   QItemSelection selitems;
 
-//std::cerr << "sel UUID " << selIds.value(0) <<"\n";
-//std::cerr << "root rowcount " << qmodel->rowCount(root) <<"\n";
     DescriptivePhrase* rPhrase = qmodel->getItem(this->rootIndex());
-//std::cerr << "root title " << rPhrase->title() <<"\n";
-//std::cerr << "root UUID " << rPhrase->relatedEntityId().toString() <<"\n";
-
   for(int row=0; row<qmodel->rowCount(this->rootIndex()); ++row)
     {
     DescriptivePhrase* dPhrase = qmodel->getItem(
      qmodel->index(row, 0, this->rootIndex()));
-//std::cout << "row UUID " << dPhrase->relatedEntityId().toString() <<"\n";
-//std::cerr << "title " << dPhrase->title() <<"\n";
-
     if(selIds.contains(dPhrase->relatedEntityId().toString()))
       {
-      std::cout << "sel UUID " << dPhrase->relatedEntityId().toString() << " title "
-      << dPhrase->title() << "\n";
       QItemSelectionRange sr(qmodel->index(row, 0, this->rootIndex()));
       selitems.append(sr);
       }
     }
   if(selitems.count())
     {
-      this->blockSignals(true);
-      this->selectionModel()->select(selitems, QItemSelectionModel::ClearAndSelect);
-      this->blockSignals(false);
-      this->scrollTo(selitems.value(0).topLeft());
-      std::cerr << "select row " << selitems.value(0).top() <<"\n";
+    this->blockSignals(true);
+    this->selectionModel()->select(selitems, QItemSelectionModel::ClearAndSelect);
+    this->blockSignals(false);
+    this->scrollTo(selitems.value(0).topLeft());
     }
 }
 
