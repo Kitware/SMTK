c3cb956c8dad6aaead73f030081b8a8e0091ccbf
David Thompson <david.thompson@kitware.com>
2014-05-24 08:11:13 -0400

    Add an Operator subclass named RemoteOperator.
    The RemoteOperator subclass is still abstract,
    but provides a basis for concrete, application-specific
    implementations of executing modeling operations remote
    to the process of the modeling kernel.

---
+++ smtk/model/RemoteOperator.cxx
@@ -0,0 +1,26 @@
+#include "smtk/model/RemoteOperator.h"
+#include "smtk/model/Parameter.h"
+#include "smtk/model/Storage.h"
+
+namespace smtk {
+  namespace model {
+
+std::string RemoteOperator::name() const
+{
+  return this->m_name;
+}
+
+Operator::Ptr RemoteOperator::cloneInternal(Operator::ConstPtr src)
+{
+  this->setName(src->name());
+  return this->Operator::cloneInternal(src);
+}
+
+RemoteOperator::Ptr RemoteOperator::setName(const std::string& opName)
+{
+  this->m_name = opName;
+  return shared_from_this();
+}
+
+  } // model namespace
+} // smtk namespace
