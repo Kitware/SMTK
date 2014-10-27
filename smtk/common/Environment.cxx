25a4e1984b18f2ee10729b7c8a111f72ba634e3e
David Thompson <david.thompson@kitware.com>
2014-10-27 18:44:51 -0400

    Add copyright statements. Ick.

--- smtk/common/Environment.cxx
+++ smtk/common/Environment.cxx
@@ -1,3 +1,14 @@
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//=============================================================================
 #include "smtk/common/Environment.h"
 
 #include <stdlib.h>
