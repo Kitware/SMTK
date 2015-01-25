25c15b371a8826805310744d540088bdc6f34f61
Yumin Yuan <yumin.yuan@kitware.com>
2015-01-25 16:39:40 -0500

    Fixed an issue with SplitFaceOperator in discrete bridge

--- smtk/bridge/discrete/SplitFaceOperator.cxx
+++ smtk/bridge/discrete/SplitFaceOperator.cxx
@@ -14,16 +14,20 @@
 
 #include "smtk/attribute/Attribute.h"
 #include "smtk/attribute/DoubleItem.h"
+#include "smtk/attribute/IntItem.h"
 #include "smtk/attribute/ModelEntityItem.h"
 
 #include "smtk/model/Operator.h"
+#include "smtk/model/Events.h"
+#include "smtk/model/Face.h"
 #include "smtk/model/ModelEntity.h"
 #include "smtk/model/Manager.h"
+#include "smtk/model/Volume.h"
 
 #include "vtkDiscreteModelWrapper.h"
 #include "vtkModel.h"
 #include "vtkModelFace.h"
-#include "vtkModelItem.h"
+#include "vtkModelRegion.h"
 #include "vtkModelEntity.h"
 
 #include "SplitFaceOperator_xml.h"
@@ -50,6 +54,29 @@ bool SplitFaceOperator::ableToOperate()
     // The CMB face to split must be valid
     this->fetchCMBFaceId() >= 0
     ;
+}
+
+/// The current discrete bridge translation from vtkDiscreteModel to smtk model
+/// does not have all the relationship set up properly, mostly the shell/face use related.
+/// Therefore we have to add the raw relationship here.
+static void internal_addRawRelationship(
+  smtk::model::Face& face,
+  smtk::model::Volume& vol1,
+  smtk::model::Volume& vol2)
+{
+  if(!face.isValid())
+    return;
+
+  if(vol1.isValid())
+    {
+    face.addRawRelation(vol1);
+    vol1.addRawRelation(face);
+    }
+  if(vol2.isValid())
+    {
+    face.addRawRelation(vol2);
+    vol2.addRawRelation(face);
+    }
 }
 
 OperatorResult SplitFaceOperator::operateInternal()
@@ -79,23 +106,53 @@ OperatorResult SplitFaceOperator::operateInternal()
     //       them in the OperatorResult (well, a subclass).
     smtk::model::ManagerPtr store = this->manager();
 
-    // First, get rid of the old face that we split.
     smtk::common::UUID faceUUID =
       this->specification()->findModelEntity("face to split")->value().entity();
+    vtkModelFace* origFace = vtkModelFace::SafeDownCast(
+      bridge->entityForUUID(faceUUID));
+
+    vtkModelRegion* v1 = origFace->GetModelRegion(0);
+    vtkModelRegion* v2 = origFace->GetModelRegion(1);
+    Volume vol1 = v1 ? Volume(store, bridge->findOrSetEntityUUID(v1)) : Volume();
+    Volume vol2 = v2 ? Volume(store, bridge->findOrSetEntityUUID(v2)) : Volume();
+
+    // First, get rid of the old face that we split.
     store->erase(faceUUID);
 
     // Now re-add it (it will have new edges)
-    vtkModelItem* item = bridge->entityForUUID(faceUUID);
-    Cursor c = bridge->addCMBEntityToManager(faceUUID, item, store, true);
+    faceUUID = bridge->findOrSetEntityUUID(origFace);
+    smtk::model::Face c = bridge->addFaceToManager(faceUUID,
+      origFace, store, true);
+
+    internal_addRawRelationship(c, vol1, vol2);
+
+    // Return the list of entities that were created
+    // so that remote bridges can track what records
+    // need to be re-fetched.
+    smtk::attribute::ModelEntityItem::Ptr resultEntities =
+      result->findModelEntity("entities");
+    resultEntities->setNumberOfValues(1);
+    resultEntities->setValue(0, c);
+
+    smtk::attribute::IntItem::Ptr eventEntity =
+      result->findInt("eventtype");
+    eventEntity->setNumberOfValues(1);
+    eventEntity->setValue(0, TESSELLATION_ENTRY);
 
     vtkIdTypeArray* newFaceIds = this->m_op->GetCreatedModelFaceIDs();
+    smtk::attribute::ModelEntityItem::Ptr newEntities =
+      result->findModelEntity("new entities");
+    newEntities->setNumberOfValues(newFaceIds->GetMaxId() + 1);
+
     for (vtkIdType i = 0; i <= newFaceIds->GetMaxId(); ++i)
       {
       vtkIdType faceId = newFaceIds->GetValue(i);
       vtkModelFace* face = dynamic_cast<vtkModelFace*>(
         modelWrapper->GetModelEntity(vtkModelFaceType, faceId));
       faceUUID = bridge->findOrSetEntityUUID(face);
-      Cursor c = bridge->addCMBEntityToManager(faceUUID, face, store, true);
+      smtk::model::Face cFace = bridge->addFaceToManager(faceUUID, face, store, true);
+      newEntities->setValue(i, cFace);
+      internal_addRawRelationship(cFace, vol1, vol2);
       /*
       this->m_op->SetCurrentNewFaceId(faceId);
       vtkIdTypeArray* spvrt = this->m_op->GetSplitEdgeVertIds();
