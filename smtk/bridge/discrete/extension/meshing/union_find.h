fe498390a6d9c3422aefeee533f7868e893a2302
Yumin Yuan <yumin.yuan@kitware.com>
2015-02-09 17:39:40 -0500

    Add missing copyright statement in a file

--- smtk/bridge/discrete/extension/meshing/union_find.h
+++ smtk/bridge/discrete/extension/meshing/union_find.h
@@ -1,3 +1,13 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+
 #ifndef __smtkdiscrete_union_find_h
 #define __smtkdiscrete_union_find_h
 
