41db163618d025ab0d6eb3ff879dd21aa6b56758
David Thompson <david.thompson@kitware.com>
2015-02-19 13:36:52 -0500

    Integration test for descriptive phrases...
    ... validating their use over multiple sessions of
    different types with multiple models loaded.
    
    This changes existing tests to accept the data-directory
    command-line argument using the new smtk.testing module
    that works with Python's unittest module (which draws
    unfortunate conclusions about command-line arguments).

---
+++ smtk/model/testing/python/integrationMultiSessionDescriptivePhrase.py
@@ -0,0 +1,93 @@
+#=============================================================================
+#
+#  Copyright (c) Kitware, Inc.
+#  All rights reserved.
+#  See LICENSE.txt for details.
+#
+#  This software is distributed WITHOUT ANY WARRANTY; without even
+#  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.  See the above copyright notice for more information.
+#
+#=============================================================================
+import os, sys
+import unittest
+import smtk
+import smtk.testing
+from smtk.simple import *
+
+class IntegrationMultiSessionDescriptivePhrase(unittest.TestCase):
+
+    def setUp(self):
+        self.session_files = {
+            'cgm': ['cgm', 'pyramid.brep'],
+            'discrete': ['cmb', 'test2D.cmb'],
+            'exodus': ['exodus', 'disk_out_ref.ex2']
+        }
+        for required in self.session_files.keys():
+            if required not in smtk.model.Manager.sessionTypeNames():
+                print 'ERROR: %s not available.' % required
+
+        self.mgr = smtk.model.Manager.create()
+        self.sessions = {}
+        for (session_type,path) in self.session_files.items():
+            sref = self.mgr.createSession(session_type)
+            sref.setName(sref.flagSummary(0))
+            SetActiveSession(sref)
+            self.sessions[session_type] = {'session_ref':sref}
+
+            filename = os.path.join(*([smtk.testing.DATA_DIR,] + path))
+            self.sessions[session_type]['entities'] = Read(filename)
+        self.mgr.assignDefaultNames()
+
+        baseline = (
+            smtk.testing.DATA_DIR,
+            'baselines',
+            'model',
+            'integrationMultiSessionDescriptivePhrases.json')
+
+        # Load the test results we expect
+        import json
+        jsdata = open(os.path.join(*baseline), 'r')
+        self.correct = json.loads(jsdata.read())
+        jsdata.close()
+
+    def recursePhrase(self, phrase, depth):
+      record = '%s (%s)' % (phrase.title(), phrase.subtitle())
+
+      if depth > 3:
+          return [record,]
+      subs = [self.recursePhrase(x, depth + 1) for x in phrase.subphrases()]
+      return [record, subs]
+
+    def printPhrases(self, indent, top):
+        for entry in top:
+            if type(entry) == str:
+              print indent + entry
+            else:
+              self.printPhrases(indent + '  ', entry)
+
+    def testPhrase(self):
+        #sessions = self.mgr.findEntitiesOfType(smtk.model.SESSION, True)
+        phrase = smtk.model.EntityListPhrase.create().setup(
+            [x[1]['session_ref'] for x in self.sessions.items()])
+        spg = smtk.model.SimpleModelSubphrases.create()
+        phrase.setDelegate(spg)
+
+        allPhrases = self.recursePhrase(phrase, 0)
+        self.printPhrases('', allPhrases)
+        self.assertEqual(allPhrases, self.correct, "Phrases mismatched.")
+
+        #Uncomment the lines below to generate a new test baseline.
+        #import json
+        #jsout = open('/tmp/phrase.json', 'w')
+        #print >>jsout, json.dumps(allPhrases)
+        #jsout.close()
+
+    def tearDown(self):
+      # Release all references to sessions.
+      # This should cause a clean shutdown of each session.
+      self.sessions = {}
+
+if __name__ == '__main__':
+  smtk.testing.process_arguments()
+  unittest.main()
--- smtk/model/testing/python/modelAttributes.py
+++ smtk/model/testing/python/modelAttributes.py
@@ -22,9 +22,11 @@ import logging
 import os
 import sys
 import uuid
+import unittest
 
 try:
     import smtk
+    import smtk.testing
 except ImportError:
     print
     print 'Not able to import smtk library. You might need to:'
@@ -45,228 +47,223 @@ SBI_FILENAME   = 'Basic2DFluid.sbi'
 
 
 # ---------------------------------------------------------------------
-def load_xref(scope, folder=None):
-  '''Parses cross-reference file to initialize lists of entity uuids.
+class TestModelAttributes(unittest.TestCase):
 
-  List has same order as entities in original (CMB) model
-  '''
-  scope.vertex_list = list()
-  scope.edge_list = list()
-  scope.face_list = list()
+  # ---------------------------------------------------------------------
+  def load_xref(self, scope, folder=None):
+    '''Parses cross-reference file to initialize lists of entity uuids.
 
-  list_map = {
-    'vertex': scope.vertex_list,
-    'edge': scope.edge_list,
-    'face': scope.face_list
-  }
+    List has same order as entities in original (CMB) model
+    '''
+    scope.vertex_list = list()
+    scope.edge_list = list()
+    scope.face_list = list()
 
-  filename = 'test2D.xref'
-  path = filename
-  if folder is not None:
-    path = os.path.join(folder, filename)
-  logging.info('Loading %s' % path)
-  done = False
-  with open(path, 'r') as f:
-    for line in f.readlines():
-      if line.startswith('Reading'):
-        continue
+    list_map = {
+      'vertex': scope.vertex_list,
+      'edge': scope.edge_list,
+      'face': scope.face_list
+    }
 
-      #print line
-      parts = line.split()
-      #print parts[2], parts[-1]
-      entity_type = parts[2]
-      uuid = parts[-1]
-      entity_list = list_map.get(entity_type)
-      entity_list.append(uuid)
-    done = True
+    filename = 'test2D.xref'
+    path = filename
+    if folder is not None:
+      path = os.path.join(folder, filename)
+    logging.info('Loading %s' % path)
+    done = False
+    with open(path, 'r') as f:
+      for line in f.readlines():
+        if line.startswith('Reading'):
+          continue
 
-  if not done:
-    logging.error('Problem loading %s' % path)
-    sys.exit(3)
+        #print line
+        parts = line.split()
+        #print parts[2], parts[-1]
+        entity_type = parts[2]
+        uuid = parts[-1]
+        entity_list = list_map.get(entity_type)
+        entity_list.append(uuid)
+      done = True
+
+    if not done:
+      logging.error('Problem loading %s' % path)
+      sys.exit(3)
 
 
-# ---------------------------------------------------------------------
-def generate_attributes(scope):
-  '''Builds and returns attribute system
+  # ---------------------------------------------------------------------
+  def generate_attributes(self, scope):
+    '''Builds and returns attribute system
 
-  Also adds boundary groups to the model
-  '''
-    # Load attribute file
-  att_folder = os.path.join(model_folder, 'attribute')
-  att_path = os.path.join(att_folder, SBT_FILENAME)
-  logging.info('Reading %s' % att_path)
-  system = smtk.attribute.System()
-  #system.setRefModelManager(scope.store)
-  reader = smtk.io.AttributeReader()
-  logger = smtk.io.Logger()
-  err = reader.read(system, att_path, logger)
-  if err:
-    logging.error("Unable to load template file")
-    logging.error(logger.convertToString())
-    sys.exit(4)
+    Also adds boundary groups to the model
+    '''
+      # Load attribute file
+    att_folder = os.path.join(self.model_folder, 'attribute')
+    att_path = os.path.join(att_folder, SBT_FILENAME)
+    logging.info('Reading %s' % att_path)
+    system = smtk.attribute.System()
+    #system.setRefModelManager(scope.store)
+    reader = smtk.io.AttributeReader()
+    logger = smtk.io.Logger()
+    err = reader.read(system, att_path, logger)
+    if err:
+      logging.error("Unable to load template file")
+      logging.error(logger.convertToString())
+      sys.exit(4)
 
-  # Create material attribute & associate to model face
-  system.setRefModelManager(scope.store)
-  defn = system.findDefinition('Material')
-  value = 1.01
-  for i,face in enumerate(scope.face_list, start=1):
-    att_name = 'material %d' % i
-    att = system.createAttribute(att_name, defn)
+    # Create material attribute & associate to model face
+    system.setRefModelManager(scope.store)
+    defn = system.findDefinition('Material')
+    value = 1.01
+    for i,face in enumerate(scope.face_list, start=1):
+      att_name = 'material %d' % i
+      att = system.createAttribute(att_name, defn)
 
-    for item_name in ['Density', 'Viscosity']:
-      item = att.find(item_name)
-      concrete_item = smtk.attribute.to_concrete(item)
-      concrete_item.setValue(0, value)
-      value += 0.491
+      for item_name in ['Density', 'Viscosity']:
+        item = att.find(item_name)
+        concrete_item = smtk.attribute.to_concrete(item)
+        concrete_item.setValue(0, value)
+        value += 0.491
 
-    face_id = uuid.UUID(face)
-    logging.debug('Associate attribute \"%s\" to face %s' % \
-      (att_name, face_id))
-    att.associateEntity(face_id)
+      face_id = uuid.UUID(face)
+      logging.debug('Associate attribute \"%s\" to face %s' % \
+        (att_name, face_id))
+      att.associateEntity(face_id)
 
-    # Save attribute name and model entity (uuid) for checking later
-    meta = (att.name(), face_id)
+      # Save attribute name and model entity (uuid) for checking later
+      meta = (att.name(), face_id)
+      scope.att_data.append(meta)
+
+    # Generate boundary groups, hard-code to specific model edges
+    flags = smtk.model.MODEL_BOUNDARY | smtk.model.DIMENSION_1
+    left_edges = scope.store.addGroup(flags, 'left_edges')
+    uuid_list = list()
+    for i in [0, 1, 2]:
+      uuid_list.append(uuid.UUID(scope.edge_list[i]))
+    scope.store.addToGroup(left_edges.entity(), uuid_list)
+
+    right_edges = scope.store.addGroup(flags, 'right_edges')
+    del uuid_list[:]
+    for i in [6, 9]:
+      uuid_list.append(uuid.UUID(scope.edge_list[i]))
+    scope.store.addToGroup(right_edges.entity(), uuid_list)
+
+    # Create boundary condition attributes
+    defn = system.findDefinition('Velocity')
+    left_att = system.createAttribute('leftBC', defn)
+    item = left_att.item(0)
+    concrete_item = smtk.attribute.to_concrete(item)
+    concrete_item.setValue(0, 3.14159)
+    concrete_item.setValue(1, 2.71828)
+    logging.debug('Associate attribute \"%s\" to boundary group %s' % \
+      (left_att.name(), left_edges.name()))
+    ok = left_att.associateEntity(left_edges.entity())
+    meta = (left_att.name(), left_edges.entity())
     scope.att_data.append(meta)
 
-  # Generate boundary groups, hard-code to specific model edges
-  flags = smtk.model.MODEL_BOUNDARY | smtk.model.DIMENSION_1
-  left_edges = scope.store.addGroup(flags, 'left_edges')
-  uuid_list = list()
-  for i in [0, 1, 2]:
-    uuid_list.append(uuid.UUID(scope.edge_list[i]))
-  scope.store.addToGroup(left_edges.entity(), uuid_list)
+    defn = system.findDefinition('Pressure')
+    right_att = system.createAttribute('rightBC', defn)
+    item = left_att.item(0)
+    concrete_item = smtk.attribute.to_concrete(item)
+    concrete_item.setValue(0, 14.1)
+    logging.debug('Associate attribute \"%s\" to boundary group %s' % \
+      (right_att.name(), right_edges.name()))
+    ok = right_att.associateEntity(right_edges.entity())
+    meta = (right_att.name(), right_edges.entity())
+    scope.att_data.append(meta)
 
-  right_edges = scope.store.addGroup(flags, 'right_edges')
-  del uuid_list[:]
-  for i in [6, 9]:
-    uuid_list.append(uuid.UUID(scope.edge_list[i]))
-  scope.store.addToGroup(right_edges.entity(), uuid_list)
-
-  # Create boundary condition attributes
-  defn = system.findDefinition('Velocity')
-  left_att = system.createAttribute('leftBC', defn)
-  item = left_att.item(0)
-  concrete_item = smtk.attribute.to_concrete(item)
-  concrete_item.setValue(0, 3.14159)
-  concrete_item.setValue(1, 2.71828)
-  logging.debug('Associate attribute \"%s\" to boundary group %s' % \
-    (left_att.name(), left_edges.name()))
-  ok = left_att.associateEntity(left_edges.entity())
-  meta = (left_att.name(), left_edges.entity())
-  scope.att_data.append(meta)
-
-  defn = system.findDefinition('Pressure')
-  right_att = system.createAttribute('rightBC', defn)
-  item = left_att.item(0)
-  concrete_item = smtk.attribute.to_concrete(item)
-  concrete_item.setValue(0, 14.1)
-  logging.debug('Associate attribute \"%s\" to boundary group %s' % \
-    (right_att.name(), right_edges.name()))
-  ok = right_att.associateEntity(right_edges.entity())
-  meta = (right_att.name(), right_edges.entity())
-  scope.att_data.append(meta)
-
-  return system
+    return system
 
 
-# ---------------------------------------------------------------------
-def check_attributes(scope, system):
-  '''Checks for attributes and associations
+  # ---------------------------------------------------------------------
+  def check_attributes(self, scope, system):
+    '''Checks for attributes and associations
 
-  Returns number of errors found
-  '''
-  error_count = 0  # return value
+    Returns number of errors found
+    '''
+    error_count = 0  # return value
 
-  for t in scope.att_data:
-    att_name, entity_uuid = t
-    #logging.debug('att_name %s, uuid %s' % t)
-    att = system.findAttribute(att_name)
-    if not att:
-      logging.error('Missing attribute %s' % att_name)
-      error_count += 1
-    entity_id_set = att.associatedModelEntityIds()
-    if not entity_id_set:
-      logging.error('Missing model entity on attribute %s' % att_name)
-      error_count += 1
+    for t in scope.att_data:
+      att_name, entity_uuid = t
+      #logging.debug('att_name %s, uuid %s' % t)
+      att = system.findAttribute(att_name)
+      if not att:
+        logging.error('Missing attribute %s' % att_name)
+        error_count += 1
+      entity_id_set = att.associatedModelEntityIds()
+      if not entity_id_set:
+        logging.error('Missing model entity on attribute %s' % att_name)
+        error_count += 1
 
-    entity_ids = list(entity_id_set)
-    entity_id = entity_ids[0]
-    # Compare uuid strings
-    if entity_id.hex != entity_uuid.hex:
-      logging.error('Unexpected model entity %s on attribute %s' % \
-        (entity_ids[0], att_name))
-      error_count += 1
+      entity_ids = list(entity_id_set)
+      entity_id = entity_ids[0]
+      # Compare uuid strings
+      if entity_id.hex != entity_uuid.hex:
+        logging.error('Unexpected model entity %s on attribute %s' % \
+          (entity_ids[0], att_name))
+        error_count += 1
 
-  logging.debug('check_attributes error_count %d' % error_count)
-  return error_count
+    logging.debug('check_attributes error_count %d' % error_count)
+    return error_count
 
 
-# ---------------------------------------------------------------------
+  def testAssociation(self):
+
+    # Define scope object to store shared data
+    ScopeType = type('Scope', (object,), dict())
+    scope = ScopeType()
+    self.model_folder = os.path.join(smtk.testing.DATA_DIR, 'smtk')
+
+    # Load the model file
+    model_path = os.path.join(self.model_folder, MODEL_FILENAME)
+    logging.info('Reading %s' % model_path)
+    json_string = None
+    with open(model_path, 'r') as f:
+      json_string = f.read()
+    if json_string is None:
+      logging.error('Unable to load input file')
+      sys.exit(2)
+    scope.store = smtk.model.Manager.create()
+    ok = smtk.io.ImportJSON.intoModelManager(json_string, scope.store)
+
+    # Load cross-reference file
+    self.load_xref(scope, self.model_folder)
+
+    # Build attributes and write to file
+    scope.att_data = list()
+    system = self.generate_attributes(scope)
+    logging.info('Writing %s' % SBI_FILENAME)
+    writer = smtk.io.AttributeWriter()
+    logger = smtk.io.Logger()
+    err = writer.write(system, SBI_FILENAME, logger)
+    if err:
+      logging.error('Unable to write attribute file')
+      logging.error(logger.convertToString())
+      sys.exit(5)
+
+    # Delete model & attributes
+    del scope.store
+    del system
+
+    # Re-import model
+    test_store = smtk.model.Manager.create()
+    ok = smtk.io.ImportJSON.intoModelManager(json_string, test_store)
+    scope.store = test_store
+
+    # Re-read attribute file
+    logging.info('Reading back %s' % SBI_FILENAME)
+    test_system = smtk.attribute.System()
+    reader = smtk.io.AttributeReader()
+    err = reader.read(test_system, SBI_FILENAME, logger)
+    if err:
+      logging.error("Unable to read attribute file")
+      logging.error(logger.convertToString())
+    self.assertTrue(not err, "Unable to read attribute file")
+
+    # Set model and verify attributes
+    test_system.setRefModelManager(scope.store)
+    error_count = self.check_attributes(scope, test_system)
+    self.assertEqual(error_count, 0, "At least one error occurred.")
+
 if __name__ == '__main__':
-  # First (and) only script argument is the path to the smtk data directory
-  if len(sys.argv) < 2:
-    print
-    print 'Test attribute association with smtk model'
-    print 'Usage: python %s path-to-SMTKTestData'
-    print
-    sys.exit(1)
-
-  # Define scope object to store shared data
-  ScopeType = type('Scope', (object,), dict())
-  scope = ScopeType()
-  smtk_test_data = sys.argv[1]
-  model_folder = os.path.join(smtk_test_data, 'smtk')
-
-  # Load the model file
-  model_path = os.path.join(model_folder, MODEL_FILENAME)
-  logging.info('Reading %s' % model_path)
-  json_string = None
-  with open(model_path, 'r') as f:
-    json_string = f.read()
-  if json_string is None:
-    logging.error('Unable to load input file')
-    sys.exit(2)
-  scope.store = smtk.model.Manager.create()
-  ok = smtk.io.ImportJSON.intoModelManager(json_string, scope.store)
-
-  # Load cross-reference file
-  load_xref(scope, model_folder)
-
-  # Build attributes and write to file
-  scope.att_data = list()
-  system = generate_attributes(scope)
-  logging.info('Writing %s' % SBI_FILENAME)
-  writer = smtk.io.AttributeWriter()
-  logger = smtk.io.Logger()
-  err = writer.write(system, SBI_FILENAME, logger)
-  if err:
-    logging.error('Unable to write attribute file')
-    logging.error(logger.convertToString())
-    sys.exit(5)
-
-  # Delete model & attributes
-  del scope.store
-  del system
-
-  # Re-import model
-  test_store = smtk.model.Manager.create()
-  ok = smtk.io.ImportJSON.intoModelManager(json_string, test_store)
-  scope.store = test_store
-
-  # Re-read attribute file
-  logging.info('Reading back %s' % SBI_FILENAME)
-  test_system = smtk.attribute.System()
-  reader = smtk.io.AttributeReader()
-  err = reader.read(test_system, SBI_FILENAME, logger)
-  if err:
-    logging.error("Unable to read attribute file")
-    logging.error(logger.convertToString())
-    sys.exit(6)
-
-  # Set model and verify attributes
-  test_system.setRefModelManager(scope.store)
-  error_count = check_attributes(scope, test_system)
-  if error_count > 0:
-    sys.exit(7)
-
-  sys.exit(0)
+    smtk.testing.process_arguments()
+    unittest.main()
