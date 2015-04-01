a025f77690676b579329ab1dc85a4fbc8bc0227d
David Thompson <david.thompson@kitware.com>
2015-04-01 16:15:30 -0400

    Add missing copyright statements.

--- smtk/extension/remus/MeshOperator.cxx
+++ smtk/extension/remus/MeshOperator.cxx
@@ -1,3 +1,14 @@
+//=============================================================================
+//
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//
+//=============================================================================
 #include "smtk/extension/remus/MeshOperator.h"
 
 #include <remus/client/Client.h>
