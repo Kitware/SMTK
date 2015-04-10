a9f1cccb0d400d876c4cb2d966a8cb72a62b2a8a
David Thompson <david.thompson@kitware.com>
2015-04-10 15:47:46 -0400

    Make discreteReadFile a unittest and document why.
    ... it is very easy to use these tests when adding new
    functionality and testing it.

--- smtk/bridge/discrete/testing/python/discreteReadFile.py
+++ smtk/bridge/discrete/testing/python/discreteReadFile.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import sys
 #=============================================================================
 #
 #  Copyright (c) Kitware, Inc.
@@ -14,32 +13,52 @@ import sys
 import os
 import sys
 import smtk
+import smtk.testing
 from smtk.simple import *
 
-mgr = smtk.model.Manager.create()
-sref = mgr.createSession('discrete', smtk.model.SessionRef())
-SetActiveSession(sref)
-sref.assignDefaultName()
+try:
+  import unittest2 as unittest
+except ImportError:
+  import unittest
 
-print '\n\n%s: type "%s" %s %s' % \
-  (sref.name(), sref.session().name(), sref.flagSummary(0), sref.session().sessionId())
-print '  Site: %s' % (sref.site() or 'local')
-for eng in sref.engines():
-  print '  Engine %s filetypes:\n    %s' % \
-    (eng, '\n    '.join(sref.fileTypes(eng)))
-print 'Operators:\n  '
-print '\n  '.join(sref.operatorNames())
-print '\n'
+class TestDiscreteSession(unittest.TestCase):
 
-mod = smtk.model.Model(
-    Read(os.path.join(sys.argv[1], 'cmb', 'test2D.cmb'))[0])
+  def setUp(self):
+    import os, sys
+    self.filename = os.path.join(smtk.testing.DATA_DIR, 'cmb', 'test2D.cmb')
 
-print '\nFree cells:\n  %s' % '\n  '.join([x.name() for x in mod.cells()])
-print '\nGroups:\n  %s\n' % '\n  '.join([x.name() for x in mod.groups()])
-if len(mod.cells()) != 4:
-  print smtk.io.ExportJSON.fromModelManager(mgr)
-  raise Exception, 'Wrong number of free cells'
+    self.mgr = smtk.model.Manager.create()
+    sess = self.mgr.createSession('discrete')
+    sess.assignDefaultName()
+    SetActiveSession(sess)
+    self.shouldSave = False
 
-out = file('test2d.json', 'w')
-print >>out, smtk.io.ExportJSON.fromModelManager(mgr)
-out.close()
+  def testFileTypes(self):
+    sref = GetActiveSession()
+    print '\n\n%s: type "%s" %s %s' % \
+      (sref.name(), sref.session().name(), sref.flagSummary(0), sref.session().sessionId())
+    print '  Site: %s' % (sref.site() or 'local')
+    for eng in sref.engines():
+      print '  Engine %s filetypes:\n    %s' % \
+        (eng, '\n    '.join(sref.fileTypes(eng)))
+    print 'Operators:\n  '
+    print '\n  '.join(sref.operatorNames())
+    print '\n'
+
+  def testRead(self):
+    mod = smtk.model.Model(Read(self.filename)[0])
+
+    print '\nFree cells:\n  %s' % '\n  '.join([x.name() for x in mod.cells()])
+    print '\nGroups:\n  %s\n' % '\n  '.join([x.name() for x in mod.groups()])
+    if len(mod.cells()) != 4:
+      print smtk.io.ExportJSON.fromModelManager(mgr)
+    self.assertEqual(len(mod.cells()), 4, 'Expected 4 free cells')
+
+    if self.shouldSave:
+      out = file('test2d.json', 'w')
+      print >>out, smtk.io.ExportJSON.fromModelManager(mgr)
+      out.close()
+
+if __name__ == '__main__':
+  smtk.testing.process_arguments()
+  unittest.main()
