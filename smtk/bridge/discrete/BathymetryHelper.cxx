77860e09ee8ea624bc7a4732bed0728fa2d3f839
Yumin Yuan <yumin.yuan@kitware.com>
2015-05-20 14:25:54 -0400

    Add bathymetry readers and helpers to discrete session

---
+++ smtk/bridge/discrete/BathymetryHelper.cxx
@@ -0,0 +1,103 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+#include "BathymetryHelper.h"
+
+#ifdef HAS_GDAL_RASTER_READER
+#include "smtk/bridge/discrete/extension/reader/vtkGDALRasterReader.h"
+#include "smtk/bridge/discrete/extension/reader/vtkGDALRasterPolydataWrapper.h"
+#endif
+
+#include "smtk/bridge/discrete/extension/reader/vtkLASReader.h"
+#include "smtk/bridge/discrete/extension/reader/vtkLIDARReader.h"
+#include "vtkDataSetSurfaceFilter.h"
+#include "vtkNew.h"
+#include "vtkPDataSetReader.h"
+#include "vtkPolyData.h"
+#include "vtkXMLPolyDataReader.h"
+#include <vtksys/SystemTools.hxx>
+
+namespace smtk {
+  namespace bridge {
+    namespace discrete {
+
+/// Private constructor since this class is a base class which should not be instantiated.
+BathymetryHelper::BathymetryHelper()
+{
+}
+
+BathymetryHelper::~BathymetryHelper()
+{
+  this->s_fileNamesToSources.clear();
+}
+
+bool BathymetryHelper::loadBathymetryFile(const std::string& filename)
+{
+  if (filename.empty())
+    {
+    std::cerr << "File name is empty!\n";
+    return false;
+    }
+
+  // get the ouptut
+  vtkNew<vtkPolyData> output;
+
+  std::string ext = vtksys::SystemTools::GetFilenameLastExtension(filename);
+  std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
+
+  if(ext == ".pts" || ext == ".bin")
+    {
+    // binary or ASCII automatically determined
+    vtkNew<vtkLIDARReader> reader;
+    reader->SetFileName( filename.c_str() );
+    reader->Update();
+    output->ShallowCopy( reader->GetOutput() );
+    }
+#ifdef HAS_GDAL_RASTER_READER
+  else if (ext == ".dem")
+    {
+    vtkNew<vtkGDALRasterPolydataWrapper> reader;
+    reader->SetFileName(filename.c_str());
+    reader->Update();
+    output->ShallowCopy( reader->GetOutput() );
+    }
+#endif
+  else if(ext == ".vtk")
+    {
+    vtkNew<vtkPDataSetReader> reader;
+    reader->SetFileName(filename.c_str());
+    reader->Update();
+
+    vtkNew<vtkDataSetSurfaceFilter> surface;
+    surface->SetInputData(0, reader->GetOutputDataObject(0));
+    surface->Update();
+
+    output->ShallowCopy( surface->GetOutput() );
+    }
+  else if(ext == ".vtp")
+    {
+    vtkNew<vtkXMLPolyDataReader> reader;
+    reader->SetFileName(filename.c_str());
+    reader->Update();
+    output->ShallowCopy( reader->GetOutput() );
+    }
+  else
+    {
+    std::cerr << "File type is not supported!\n";
+    return false;
+    }
+
+  this->s_fileNamesToSources[filename] = output.GetPointer();
+
+  return true;
+}
+
+    } // namespace discrete
+  } // namespace bridge
+} // namespace smtk
---
+++ smtk/bridge/discrete/BathymetryHelper.h
@@ -0,0 +1,55 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+#ifndef __smtk_model_BathymetryHelper_h
+#define __smtk_model_BathymetryHelper_h
+
+#include "smtk/PublicPointerDefs.h"
+#include "smtk/bridge/discrete/Exports.h" // for SMTKDISCRETESESSION_EXPORT
+#include "smtk/SharedFromThis.h" // for smtkTypeMacro
+#include "vtkSmartPointer.h"
+
+class vtkPointSet;
+
+namespace smtk {
+  namespace bridge {
+    namespace discrete {
+
+/**\brief Helper class for loading and caching bathymetry files for session.
+  *
+  * This class will read the following files and cache their output:
+  * "LIDAR (*.pts *.bin *.bin.pts);;LAS (*.las);;DEM (*.hdr *.FLT *.ftw);;VTK files (*.vtk)"
+  */
+class SMTKDISCRETESESSION_EXPORT BathymetryHelper
+{
+public:
+  smtkTypeMacro(BathymetryHelper);
+  virtual ~BathymetryHelper();
+
+  virtual bool loadBathymetryFile(const std::string& filename);
+
+//  vtkPointSet* bathymetryData(const std::string& filename);
+//  std::vector<std::string> loadedBathymetryFiles() const;
+
+protected:
+  friend class Session;
+  BathymetryHelper();
+
+  std::map<std::string, vtkSmartPointer<vtkPointSet> > s_fileNamesToSources;
+
+private:
+  BathymetryHelper(const BathymetryHelper& other); // Not implemented.
+  void operator = (const BathymetryHelper& other); // Not implemented.
+};
+
+    } // namespace discrete
+  } // namespace bridge
+} // namespace smtk
+
+#endif // __smtk_model_BathymetryHelper_h
