9bac9c7d1b9e6604562143e223f15302fc8747c3
Ben Boeckel <ben.boeckel@kitware.com>
2015-07-24 15:47:53 -0400

    cdash: update cdash site

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "SuperBuild-ConceptualModelBuilder")
 set(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.kitware.com")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=CMB")
+set(CTEST_DROP_SITE "open.cdash.org")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=SMTK")
 set(CTEST_DROP_SITE_CDASH TRUE)
