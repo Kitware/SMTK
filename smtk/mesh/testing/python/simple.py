80ea5e812dcf87089931f8e4f87bb01661c2ef4e
Robert Maynard <robert.maynard@kitware.com>
2015-10-14 09:48:42 -0400

    Add some smtk::mesh python tests.

---
+++ smtk/mesh/testing/python/simple.py
@@ -0,0 +1,47 @@
+#=============================================================================
+#
+#  Copyright (c) Kitware, Inc.
+#  All rights reserved.
+#  See LICENSE.txt for details.
+#
+#  This software is distributed WITHOUT ANY WARRANTY; without even
+#  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.  See the above copyright notice for more information.
+#
+#=============================================================================
+
+
+import os
+import smtk
+import smtk.testing
+import sys
+
+
+def test_file_load():
+    m = smtk.mesh.Manager.create()
+
+    # Load the mesh file
+    print 'data_dir', smtk.testing.DATA_DIR
+    mesh_path = os.path.join(smtk.testing.DATA_DIR, 'mesh', 'sixth_hexflatcore.h5m')
+    c = smtk.io.ImportMesh.entireFile(mesh_path,m)
+    print c
+    print c.numberOfMeshes()
+
+    #now dump some very basic info about the collection
+    print c.meshes( ).size()
+    print c.cells().size()
+    print c.points().size()
+
+    #now dump some very basic info about all the meshes
+    m = c.meshes()
+    print 'info on all meshes'
+    print len(m.domains())
+    print len(m.dirichlets())
+    print len(m.neumanns())
+    print m.cells().size()
+    print m.points().size()
+
+
+if __name__ == '__main__':
+  smtk.testing.process_arguments()
+  test_file_load()
