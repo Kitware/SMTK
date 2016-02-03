88221687a5a27169667779d16c980fea390af613
Robert Maynard <robert.maynard@kitware.com>
2016-02-03 16:54:04 -0500

    Update ModelToMesh to support PolyLine conversion to multiple mesh::Lines.

--- smtk/io/ModelToMesh.cxx
+++ smtk/io/ModelToMesh.cxx
@@ -27,6 +27,7 @@ namespace io {
 namespace detail
 {
 
+//----------------------------------------------------------------------------
 smtk::mesh::CellType tessToSMTKCell(smtk::model::Tessellation::size_type cell_shape)
 {
   smtk::mesh::CellType ctype = smtk::mesh::CellType_MAX;
@@ -36,8 +37,8 @@ smtk::mesh::CellType tessToSMTKCell(smtk::model::Tessellation::size_type cell_sh
     case smtk::model::TESS_TRIANGLE:        ctype = smtk::mesh::Triangle; break;
     case smtk::model::TESS_QUAD:            ctype = smtk::mesh::Quad; break;
     case smtk::model::TESS_POLYGON:         ctype = smtk::mesh::Polygon; break;
+    case smtk::model::TESS_POLYLINE:        ctype = smtk::mesh::Line; break;
     case smtk::model::TESS_POLYVERTEX:
-    case smtk::model::TESS_POLYLINE:
     default:
       //Polyvertex and polyline are currently un supported by smtk mesh
       ctype = smtk::mesh::CellType_MAX;
@@ -46,6 +47,7 @@ smtk::mesh::CellType tessToSMTKCell(smtk::model::Tessellation::size_type cell_sh
   return ctype;
 }
 
+//----------------------------------------------------------------------------
 void removeOnesWithoutTess(smtk::model::EntityRefs& ents)
 {
   smtk::model::EntityIterator it;
@@ -64,9 +66,9 @@ void removeOnesWithoutTess(smtk::model::EntityRefs& ents)
     {
     ents.erase(*i);
     }
-
 }
 
+//----------------------------------------------------------------------------
 template<typename MappingType>
 bool convert_vertices(const smtk::model::EntityRefs& ents,
                       MappingType& mapping,
@@ -122,7 +124,6 @@ bool convert_vertices(const smtk::model::EntityRefs& ents,
 
     //while a little more complex, this way avoids branching or comparisons
     //against dimension while filling the memory
-
     if(dimension == 3)
       {
       for( std::size_t i=0; i < length; i+=3, pos++)
@@ -139,7 +140,7 @@ bool convert_vertices(const smtk::model::EntityRefs& ents,
         meshCoords[0][pos] = modelCoords[i];
         meshCoords[1][pos] = modelCoords[i+1];
         }
-      std::fill( meshCoords[2], meshCoords[2] + numPointsToAlloc, double(0));
+      std::fill( meshCoords[2], meshCoords[2] + length, double(0));
       }
     else if(dimension == 1)
       {
@@ -147,8 +148,8 @@ bool convert_vertices(const smtk::model::EntityRefs& ents,
         {
         meshCoords[0][pos] = modelCoords[i];
         }
-      std::fill( meshCoords[1], meshCoords[1] + numPointsToAlloc, double(0));
-      std::fill( meshCoords[2], meshCoords[2] + numPointsToAlloc, double(0));
+      std::fill( meshCoords[1], meshCoords[1] + length, double(0));
+      std::fill( meshCoords[2], meshCoords[2] + length, double(0));
       }
     }
 
@@ -156,6 +157,57 @@ bool convert_vertices(const smtk::model::EntityRefs& ents,
   //into the mesh database.
   return (pos == numPointsToAlloc);
 
+}
+
+//----------------------------------------------------------------------------
+template<typename HandleData>
+void convert_fixed_size_cell(std::vector<int>& cell_conn,
+                         smtk::mesh::CellType cellType,
+                         smtk::model::Tessellation::size_type numVerts,
+                         std::vector<std::size_t>& numCellsOfType,
+                         std::vector<HandleData>& cellMBConn,
+                         std::size_t global_coordinate_offset
+                         )
+{
+  int idx = numCellsOfType[cellType]++;
+
+  smtk::mesh::Handle* currentConnLoc = cellMBConn[cellType].second + numVerts * idx;
+  for (int j=0; j < numVerts; ++j)
+    {
+    //we need to fix the cell_conn to account for the fact that the
+    //values contained within are all relative to the tessellation and
+    //we need location based on the total sum of all tessellations.
+    //local position + ( total connectivity added before the tess ) should
+    //be correct.
+    currentConnLoc[j] = global_coordinate_offset + cell_conn[j];
+    }
+}
+
+//----------------------------------------------------------------------------
+template<typename HandleData>
+void convert_poly_line(std::vector<int>& cell_conn,
+                       smtk::mesh::CellType cellType,
+                       smtk::model::Tessellation::size_type numCellsInPolyCell,
+                       std::vector<std::size_t>& numCellsOfType,
+                       std::vector<HandleData>& cellMBConn,
+                       std::size_t global_coordinate_offset
+                       )
+{
+  //numVerts represents the number of verts in the tessellation, not
+  //the number of verts in the mesh cell
+  int previous_id = numCellsOfType[cellType];
+  numCellsOfType[cellType] += numCellsInPolyCell;
+
+  int numVertsPerCell = smtk::mesh::verticesPerCell(cellType);
+  smtk::mesh::Handle* currentConnLoc = cellMBConn[cellType].second + (numVertsPerCell * previous_id);
+
+  int i=0;
+  for(int j=0; j < numCellsInPolyCell; ++j)
+    {
+    currentConnLoc[i] = global_coordinate_offset + cell_conn[j];
+    currentConnLoc[i+1] = global_coordinate_offset + cell_conn[j+1];
+    i+=2;
+    }
 }
 
 //----------------------------------------------------------------------------
@@ -180,13 +232,12 @@ convert_cells(const smtk::model::EntityRefs& ents,
     //we filtered out all ents without tess already, so this can't be null
     const Tess* tess = it->hasTessellation();
 
-    // Convert the connectivity of this volume tesselation to a meshset.
+    // Convert the connectivity of this volume tessellation to a meshset.
     // We make 2 passes through the tessellation: one to determine the
     // allocation and another to create the cells. Note that each cell
     // type must be put in a separate handle range.
     //
-    // TODO: This does not handle triangle strips/fans or other cell types
-    //       with a varying number of vertices per cell.
+    // TODO: This does not handle triangle strips/fans, or polygons
     Tess::size_type start_off;
     std::vector<std::size_t> numCellsOfType(smtk::mesh::CellType_MAX, 0);
     for (start_off = tess->begin();
@@ -195,12 +246,20 @@ convert_cells(const smtk::model::EntityRefs& ents,
          //from end means that we properly handle runs of the same cell type
       {
       Tess::size_type cell_type;
-      tess->numberOfCellVertices(start_off, &cell_type);
+      Tess::size_type numVertsPerCell = tess->numberOfCellVertices(start_off, &cell_type);
       Tess::size_type cell_shape = tess->cellShapeFromType(cell_type);
       if(cell_shape == smtk::model::TESS_VERTEX   ||
          cell_shape == smtk::model::TESS_TRIANGLE ||
          cell_shape == smtk::model::TESS_QUAD)
+        {
         numCellsOfType[tessToSMTKCell(cell_shape)]++;
+        }
+      else if(cell_shape == smtk::model::TESS_POLYLINE)
+        {
+        //In a polyline the number of cells is equal to one less than the
+        //number of points
+        numCellsOfType[tessToSMTKCell(cell_shape)] += numVertsPerCell - 1;
+        }
       }
 
     // Allocate handles.
@@ -235,36 +294,24 @@ convert_cells(const smtk::model::EntityRefs& ents,
       {
       //fetch the number of cell vertices, and the cell type in a single query
       Tess::size_type cell_type;
-      Tess::size_type numVertsPerCell = tess->numberOfCellVertices(start_off, &cell_type);
+      Tess::size_type numVerts = tess->numberOfCellVertices(start_off, &cell_type);
       Tess::size_type cell_shape = tess->cellShapeFromType(cell_type);
 
       //convert from tess type to smtk cell type
       const smtk::mesh::CellType cellType = tessToSMTKCell( cell_shape );
-
-      //the most efficient way to allocate is to do batch allocation, so lets
-      //iterate all cells of the same shape. This can only be done for
-      //points, triangles and quads, as everything else has variable cell length
-      if(cell_shape != smtk::model::TESS_VERTEX   &&
-         cell_shape != smtk::model::TESS_TRIANGLE &&
-         cell_shape != smtk::model::TESS_QUAD)
-        {
-        continue;
-        }
-
-      int idx = numCellsOfType[cellType]++;
       std::size_t global_coordinate_offset = mapping[*it];
 
-      smtk::mesh::Handle* currentConnLoc = cellMBConn[cellType].second + numVertsPerCell * idx;
-      cell_conn.reserve(numVertsPerCell);
+      cell_conn.reserve(numVerts);
       tess->vertexIdsOfCell(start_off, cell_conn);
-      for (int j=0; j < numVertsPerCell; ++j)
+      if(cell_shape == smtk::model::TESS_VERTEX   ||
+         cell_shape == smtk::model::TESS_TRIANGLE ||
+         cell_shape == smtk::model::TESS_QUAD)
         {
-        //we need to fix the cell_conn to account for the fact that the
-        //values contained within are all relative to the tessellation and
-        //we need location based on the total sum of all tessellations.
-        //local position + ( total connectivity added before the tess ) should
-        //be correct.
-        currentConnLoc[j] = global_coordinate_offset + cell_conn[j];
+        convert_fixed_size_cell(cell_conn, cellType, numVerts, numCellsOfType, cellMBConn, global_coordinate_offset);
+        }
+      else if(cell_shape == smtk::model::TESS_POLYLINE)
+        {
+        convert_poly_line(cell_conn, cellType, (numVerts-1), numCellsOfType, cellMBConn, global_coordinate_offset);
         }
 
       //this is horribly important. vertexIdsOfCell is implemented by using
@@ -281,15 +328,6 @@ convert_cells(const smtk::model::EntityRefs& ents,
       {
       if (*cellsOfTypeIt <= 0)
         continue;
-
-      smtk::mesh::CellType cellType = static_cast<smtk::mesh::CellType>(ctype);
-      int numVertsPerCell = smtk::mesh::verticesPerCell(cellType);
-
-      // notify database that we have written to connectivity, that way
-      // it can properly update adjacencies and other database info
-      ialloc->connectivityModified(allocIt->first,
-                                   numVertsPerCell,
-                                   allocIt->second);
 
       //we need to add these cells to the range that represents all
       //cells for this volume
