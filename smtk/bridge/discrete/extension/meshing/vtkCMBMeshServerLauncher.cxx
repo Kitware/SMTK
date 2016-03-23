4bcc0312b57c4f7b7093fea2f17073b3c6a1aeb7
Robert Maynard <robert.maynard@kitware.com>
2016-03-23 10:59:39 -0400

    Defer executable and relative path lookups to remus.
    remus executable path detection is far more robus on linux and windows than
    kwsys, and also provides the proper OSX bundle path detection. So instead
    of repeating the same logic in smtk, lets defer to remus.

--- smtk/bridge/discrete/extension/meshing/vtkCMBMeshServerLauncher.cxx
+++ smtk/bridge/discrete/extension/meshing/vtkCMBMeshServerLauncher.cxx
@@ -21,84 +21,12 @@
 #include <string>
 #include <vtksys/SystemTools.hxx>
 
+#include <remus/common/LocateFile.h>
 #include <remus/server/Server.h>
 #include <remus/server/WorkerFactory.h>
 
-namespace smtk {
-  namespace bridge {
-    namespace discrete {
-
-namespace resources
+namespace
 {
-inline std::string getExecutableLocation()
-  {
-  //we default our guess of the executable location to be the current working
-  //directory
-  std::string execLocation(vtksys::SystemTools::GetCurrentWorkingDirectory());
-#ifdef __APPLE__
-  CFBundleRef mainBundle = CFBundleGetMainBundle();
-  if(mainBundle)
-    {
-    CFURLRef url (CFBundleCopyExecutableURL(mainBundle));
-    if(url)
-      {
-      CFStringRef posixUrl (CFURLCopyFileSystemPath(url,kCFURLPOSIXPathStyle));
-      if(posixUrl)
-        {
-        int len = static_cast<int>(CFStringGetLength(posixUrl));
-        int bufferSize = static_cast<int>(CFStringGetMaximumSizeForEncoding(len,
-                                          kCFStringEncodingUTF8));
-        if(len > 0 && bufferSize > 0)
-          {
-          //don't use len, that returns the len for utf16, which isn't accurate
-          //you need to get the maxium size for the c string encoding
-          char* name = new char[bufferSize+1];
-          bool nameCopied = CFStringGetCString(posixUrl,name,bufferSize,
-                                               kCFStringEncodingUTF8);
-          if(nameCopied)
-            {
-            //we have a valid bundle path, copy to string and use
-            //vtksys to drop the executables name.
-            std::string execFile = std::string(name);
-            execLocation = vtksys::SystemTools::GetFilenamePath(execFile);
-            }
-          delete[] name;
-          }
-        CFRelease(posixUrl);
-        }
-      CFRelease(url);
-      }
-    }
-#endif
-  return execLocation;
-}
-
-inline std::vector<std::string> locationsToSearch()
-{
-  std::vector<std::string> locations;
-  locations.push_back("bin/");
-  locations.push_back("../");
-  locations.push_back("../bin/");
-  locations.push_back("../../bin/");
-  locations.push_back("../../");
-  locations.push_back("../../../bin/");
-  locations.push_back("../../../");
-
-#ifdef _WIN32
-  //only search paths that make sense on window development or dashboard machines
-  locations.push_back("../../../bin/Debug/");
-  locations.push_back("../../../bin/Release/");
-  locations.push_back("../../bin/Debug/");
-  locations.push_back("../../bin/Release/");
-  locations.push_back("../bin/Debug/");
-  locations.push_back("../bin/Release/");
-  locations.push_back("bin/Debug/");
-  locations.push_back("bin/Release/");
-#endif
-
-  return locations;
-}
-
 //the goal here is to find all the places that the mesh workers config
 //files could be located and give them to the factory.
 inline void locationsToSearchForWorkers(
@@ -107,11 +35,11 @@ inline void locationsToSearchForWorkers(
   //first we search from the executables locations,
   //if than we search the current working directory if it is a different path
   //than the executable location
-  std::string execLoc = resources::getExecutableLocation();
+  std::string execLoc = remus::common::getExecutableLocation();
   factory->addWorkerSearchDirectory(execLoc);
 
   typedef std::vector<std::string>::const_iterator It;
-  std::vector<std::string> locations = resources::locationsToSearch();
+  std::vector<std::string> locations = remus::common::relativeLocationsToSearch();
 
   for(It i = locations.begin(); i != locations.end();++i)
     {
@@ -122,6 +50,11 @@ inline void locationsToSearchForWorkers(
 }
 
 }
+
+
+namespace smtk {
+  namespace bridge {
+    namespace discrete {
 
 vtkStandardNewMacro(vtkCMBMeshServerLauncher)
 
@@ -134,7 +67,7 @@ vtkCMBMeshServerLauncher::vtkCMBMeshServerLauncher()
   boost::shared_ptr<remus::server::WorkerFactory> factory(
                                         new remus::server::WorkerFactory());
   factory->setMaxWorkerCount(2);
-  resources::locationsToSearchForWorkers(factory);
+  locationsToSearchForWorkers(factory);
 
   //this sets up the server but it isn't accepting any connections,
   //but has bound to the correct ports
