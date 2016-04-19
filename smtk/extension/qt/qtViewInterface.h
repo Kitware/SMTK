42801cb1d73f7620155a269804f486a3b40ae7f7
Yumin Yuan <yumin.yuan@kitware.com>
2016-04-19 09:39:10 -0400

    Added a UI interface for view and changed deprecated qt cmake call
    A UI view interface is added so that new UI view can be registered
    to the uiManager when loading a plugin if the plugin has a customized view that derives
    from the view interface. Also replaced the deprecated qt4_use_modules with proper cmake code.

---
+++ smtk/extension/qt/qtViewInterface.h
@@ -0,0 +1,49 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+
+#ifndef __smtk_attribute_qtViewInterface_h
+#define __smtk_attribute_qtViewInterface_h
+
+#include "smtk/extension/qt/Exports.h"
+#include "smtk/extension/qt/qtBaseView.h"
+
+#include <QString>
+#include <QtPlugin>
+
+typedef smtk::attribute::qtBaseView* (*qtSMTKViewConstructor)(const smtk::attribute::ViewInfo &info);
+
+namespace smtk
+{
+  namespace attribute
+  {
+
+  class qtBaseView;
+
+  /// interface class for plugins that add a QDockWindow
+  class SMTKQTEXT_EXPORT qtViewInterface
+  {
+  public:
+    qtViewInterface();
+    virtual ~qtViewInterface();
+
+    virtual QString viewName() const = 0;
+
+    /// return a static constructor for derived class of qtBaseView 
+    virtual qtSMTKViewConstructor viewConstructor() = 0;
+  private:
+    Q_DISABLE_COPY(qtViewInterface)
+  };
+
+  }; // namespace attribute
+}; // namespace smtk
+
+Q_DECLARE_INTERFACE(smtk::attribute::qtViewInterface, "com.kitware/paraview/smtkview")
+
+#endif
