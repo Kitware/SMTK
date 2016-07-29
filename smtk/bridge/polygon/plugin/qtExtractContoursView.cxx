3819ec52b210aec0a60116d6b66e81d2050245df
Yumin Yuan <yumin.yuan@kitware.com>
2016-07-29 13:09:55 -0400

    Add UI stuff for extracting contours from image in polygon session

---
+++ smtk/bridge/polygon/plugin/qtExtractContoursView.cxx
@@ -0,0 +1,343 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+
+#include "qtExtractContoursView.h"
+
+#include "smtk/attribute/Attribute.h"
+#include "smtk/attribute/IntItem.h"
+#include "smtk/attribute/StringItem.h"
+#include "smtk/extension/qt/qtAttribute.h"
+#include "smtk/extension/qt/qtModelOperationWidget.h"
+#include "smtk/extension/qt/qtModelView.h"
+#include "smtk/extension/qt/qtUIManager.h"
+#include "smtk/extension/paraview/widgets/pqGenerateContoursDialog.h"
+#include "smtk/common/View.h"
+#include "smtk/model/Operator.h"
+
+#include "pqActiveObjects.h"
+#include "pqApplicationCore.h"
+#include "pqPipelineSource.h"
+#include "pqRenderView.h"
+#include "pqServer.h"
+
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPointer>
+#include <QPushButton>
+#include <QMessageBox>
+
+using namespace smtk::extension;
+
+//----------------------------------------------------------------------------
+class qtExtractContoursViewInternals
+{
+public:
+  qtExtractContoursViewInternals()
+    {
+    }
+  ~qtExtractContoursViewInternals()
+    {
+    if(ArcManager)
+      delete ArcManager;
+    if(CurrentAtt)
+      delete CurrentAtt;
+    }
+
+  QPointer<pqGenerateContoursDialog> ContoursDialog;
+  QPointer<qtAttribute> CurrentAtt;
+  QPointer<QVBoxLayout> EditorLayout;
+
+  smtk::weak_ptr<smtk::model::Operator> CurrentOp;
+};
+
+//----------------------------------------------------------------------------
+qtBaseView *
+qtExtractContoursView::createViewWidget(const ViewInfo &info)
+{
+  qtExtractContoursView *view = new qtExtractContoursView(info);
+  view->buildUI();
+  return view;
+}
+
+//----------------------------------------------------------------------------
+qtExtractContoursView::
+qtExtractContoursView(const ViewInfo &info) :
+  qtBaseView(info)
+{
+  this->Internals = new qtExtractContoursViewInternals;
+}
+
+//----------------------------------------------------------------------------
+qtExtractContoursView::~qtExtractContoursView()
+{
+  delete this->Internals;
+}
+//----------------------------------------------------------------------------
+void qtExtractContoursView::createWidget( )
+{
+  smtk::common::ViewPtr view = this->getObject();
+  if (!view)
+    {
+    return;
+    }
+
+  QVBoxLayout* parentlayout = static_cast<QVBoxLayout*> (
+    this->parentWidget()->layout());
+  if(this->Widget)
+    {
+    if(parentlayout)
+      {
+      parentlayout->removeWidget(this->Widget);
+      }
+    delete this->Widget;
+    }
+
+  this->Widget = new QPushButton(this->parentWidget());
+  this->Widget->setObjectName("polygonStartContourButton");
+  this->Widget->setText("Generate Contour From Image");
+  this->updateAttributeData();
+
+  QObject::connect(this->Widget,
+    SIGNAL(clicked()),
+    this, SLOT(startContourOperation()));
+}
+
+inline qtAttribute* internal_createAttUI(
+  smtk::attribute::AttributePtr att, QWidget* pw, qtBaseView* view)
+{
+  if(att && att->numberOfItems()>0)
+    {
+    qtAttribute* attInstance = new qtAttribute(att, pw, view);
+    if(attInstance && attInstance->widget())
+      {
+      //Without any additional info lets use a basic layout with model associations
+      // if any exists
+      attInstance->createBasicLayout(true);
+      attInstance->widget()->setObjectName("polygonContourOpEditor");
+      QVBoxLayout* parentlayout = static_cast<QVBoxLayout*> (pw->layout());
+      parentlayout->insertWidget(0, attInstance->widget());
+      }
+    return attInstance;
+    }
+  return NULL;
+}
+
+inline vtkSMProxy* internal_createVTKContourOperator(
+  vtkSMProxy *sourceProxy)
+{
+  vtkSMProxy* smPolyEdgeOp = vtkSMProxyManager::GetProxyManager()->NewProxy(
+    "polygon_operators", "PolygonContourOperator");
+  if(!smPolyEdgeOp)
+    return NULL;
+  smPolyEdgeOp->UpdateVTKObjects();
+  sourceProxy->UpdateVTKObjects();
+
+  vtkClientServerStream stream;
+  stream  << vtkClientServerStream::Invoke
+          << VTKOBJECT(sourceProxy) << "GetOutputDataObject"
+          << 0
+          << vtkClientServerStream::End;
+  smPolyEdgeOp->GetSession()->ExecuteStream(smPolyEdgeOp->GetLocation(), stream);
+
+  stream  << vtkClientServerStream::Invoke
+          << VTKOBJECT(smPolyEdgeOp) << "SetContourInput"
+          << smPolyEdgeOp->GetSession()->GetLastResult(vtkProcessModule::DATA_SERVER_ROOT).GetArgument(0,0)
+          << vtkClientServerStream::End;
+  smPolyEdgeOp->GetSession()->ExecuteStream(smPolyEdgeOp->GetLocation(), stream);
+  return smPolyEdgeOp;
+}
+
+//----------------------------------------------------------------------------
+void qtExtractContoursView::updateAttributeData()
+{
+  smtk::common::ViewPtr view = this->getObject();
+  if (!view || !this->Widget)
+    {
+    return;
+    }
+
+  if(this->Internals->CurrentAtt)
+    {
+    delete this->Internals->CurrentAtt;
+    }
+
+  int i = view->details().findChild("AttributeTypes");
+  if(i < 0)
+    {
+    return;
+    }
+  smtk::common::View::Component& comp = view->details().child(i);
+  // for now, we only handle "edit edge" operator; later we could use a list
+  // to show all operators (attributes), and a panel underneath to edit current
+  // selected operator.
+  std::string defName;
+  for(int ci = 0; ci < comp.numberOfChildren(); ++ci)
+    {
+    smtk::common::View::Component &attComp = comp.child(ci);
+    if (attComp.name() != "Att")
+      {
+      continue;
+      }
+    std::string optype;
+    if(attComp.attribute("Type", optype) &&
+      (optype == "extract contours"))
+      {
+      defName = optype;
+      break;
+      }
+    }
+  if(defName.empty())
+    {
+    return;
+    }
+
+  smtk::model::OperatorPtr edgeOp = this->uiManager()->activeModelView()->
+                       operatorsWidget()->existingOperator(defName);
+  this->Internals->CurrentOp = edgeOp;
+  // expecting only 1 instance of the op?
+  smtk::attribute::AttributePtr att = edgeOp->specification();
+  this->Internals->CurrentAtt = internal_createAttUI(att, this->Widget, this);
+
+}
+//----------------------------------------------------------------------------
+void qtExtractContoursView::startContourOperation()
+{
+  this->operationSelected(this->Internals->CurrentOp.lock());
+}
+
+//----------------------------------------------------------------------------
+void qtExtractContoursView::requestOperation(const smtk::model::OperatorPtr& op)
+{
+  if(!op || !op->specification())
+    {
+    return;
+    }
+  this->uiManager()->activeModelView()->requestOperation(op, false);
+}
+
+//----------------------------------------------------------------------------
+void qtExtractContoursView::cancelOperation(const smtk::model::OperatorPtr& op)
+{
+  if( !op || !this->Widget || !this->Internals->CurrentAtt )
+    return;
+  if(this->Internals->ContoursDialog)
+    {
+    this->Internals->ContoursDialog->close();
+    }
+}
+
+//----------------------------------------------------------------------------
+void qtExtractContoursView::acceptContours(pqPipelineSource* contourSource)
+{
+  if(!contourSource || !this->Internals->CurrentAtt ||
+     !this->Widget || !this->Internals->CurrentOp.lock())
+    {
+    return;
+    }
+
+  smtk::attribute::AttributePtr spec = this->Internals->CurrentOp.lock()->specification();
+  if(spec->type() != "extract contours")
+    return NULL;
+  smtk::attribute::IntItem::Ptr opProxyIdItem = spec->findInt("HelperGlobalID");
+  if(!opProxyIdItem)
+    return NULL;
+  vtkSMProxy* smPolyEdgeOp = internal_createVTKContourOperator(contourSource->getProxy());
+  if(!smPolyEdgeOp)
+    return NULL;
+  // Now set the GlobalId of smPolyEdgeOp proxy to the edge op, and later
+  // on the GlobalId will be used to find the proxy
+    // for Create and Edit operation, we need arc source
+  opProxyIdItem->setValue(smPolyEdgeOp->GetGlobalID());
+  return smPolyEdgeOp;
+
+}
+
+pqPipelineSource* internal_createImageSource(const std::string& imageurl)
+{
+  pqApplicationCore* core = pqApplicationCore::instance();
+  pqObjectBuilder* builder = core->getObjectBuilder();
+  pqServer* server = core->getActiveServer();
+  builder->blockSignals(true);
+
+  QStringList files;
+  files << imageurl.c_str();
+
+  pqPipelineSource* source;
+  QFileInfo finfo(imageurl.c_str());
+  if (finfo.completeSuffix().toLower() == "tif" || finfo.completeSuffix().toLower() == "dem")
+    {
+    source =  builder->createReader("sources", "GDALRasterReader", files, server);
+    }
+  else
+    {
+    source =  builder->createReader("sources", "XMLImageDataReader", files, server);
+    }
+  builder->blockSignals(false);
+
+  if(source)
+    {
+    source = builder->createFilter("filters", "cmbStructedToMesh", source);
+    vtkSMPropertyHelper(source->getProxy(), "UseScalerForZ").Set(0);
+    source->getProxy()->UpdateVTKObjects();
+    }
+
+  return source;
+}
+
+//----------------------------------------------------------------------------
+void qtExtractContoursView::operationSelected(const smtk::model::OperatorPtr& op)
+{
+  if(!this->Internals->CurrentAtt || !this->Widget || op->name() != "extract contours")
+    return;
+
+  if(this->Internals->ContoursDialog)
+    {
+    this->Internals->ContoursDialog->close();
+    delete this->Internals->ContoursDialog;
+    }
+
+  smtk::attribute::AttributePtr spec = op->specification();
+  smtk::attribute::ModelEntityItem::Ptr modelItem = spec->associations();
+  smtk::model::Model model(modelItem->value(0));
+  if (!model.isValid())
+    {
+    qCritical() << "No model is associated with the operator.\n";
+    return;
+    }
+  smtk::model::StringList const& urlprop(model.stringProperty("image_url"));
+  if (urlprop.empty())
+    {
+    qCritical() << "No image is associated with the model, use \"import image\" operator first.\n";
+    return;
+    }
+  std::string imagefile = urlprop[0];
+  if(pqPipelineSource* source = internal_createImageSource(imagefile))
+    {
+    this->Internals->ContoursDialog = new pqGenerateContoursDialog(source,
+      this->parentWidget());
+    this->Internals->ContoursDialog->setModal(true);
+    this->Internals->ContoursDialog->exec();
+    QObject::connect(this->Internals->ContoursDialog,
+          SIGNAL(contoursAccepted(pqPipelineSource*)),
+          this, SLOT(acceptContours(pqPipelineSource*)));
+    }
+}
+
+//----------------------------------------------------------------------------
+void qtExtractContoursView::showAdvanceLevelOverlay(bool show)
+{
+  this->qtBaseView::showAdvanceLevelOverlay(show);
+}
+
+//----------------------------------------------------------------------------
+void qtExtractContoursView::requestModelEntityAssociation()
+{
+  this->updateAttributeData();
+}
---
+++ smtk/extension/paraview/widgets/pqGenerateContoursDialog.h
@@ -0,0 +1,90 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+// .NAME pqGenerateContoursDialog - .
+// .SECTION Description
+// .SECTION Caveats
+
+#ifndef __smtk_pq_GenerateContoursDialog_h
+#define __smtk_pq_GenerateContoursDialog_h
+
+#include "smtk/extension/paraview/widgets/Exports.h"
+#include "smtk/PublicPointerDefs.h"
+#include <QDialog>
+#include <QDoubleValidator>
+
+class pqDataRepresentation;
+class pqRenderView;
+class pqPipelineSource;
+class QDoubleValidator;
+class QIntValidator;
+class QProgressDialog;
+
+namespace Ui
+{
+  class qtGenerateContoursDialog;
+};
+
+class SMTKPQWIDGETSEXT_EXPORT pqGenerateContoursDialog : public QDialog
+{
+  Q_OBJECT
+public:
+  pqGenerateContoursDialog(pqPipelineSource*,
+    QWidget *parent = NULL, Qt::WindowFlags flags= 0);
+  virtual ~pqGenerateContoursDialog();
+
+  int exec();
+  int close();
+
+signals:
+  void contoursAccepted(pqPipelineSource*);
+
+protected slots:
+  void generateContours();
+  void onAccecptContours();
+  void onCancel();
+  void updateProgress(const QString&, int progress);
+  void updateContourButtonStatus();
+
+  void onOpacityChanged(int opacity);
+
+protected:
+  void setupProgressBar(QWidget* progressWidget);
+  void disableWhileProcessing();
+
+  Ui::qtGenerateContoursDialog *InternalWidget;
+
+  QDialog *MainDialog;
+  pqRenderView *RenderView;
+  pqPipelineSource *ImageSource;
+  pqDataRepresentation* ImageRepresentation;
+
+  pqDataRepresentation *ContourRepresentation;
+  pqPipelineSource *ContourSource;
+  pqPipelineSource *CleanPolyLines;
+  QString ProgressMessage;
+  bool ProgressMessagesMustMatch;
+  double ContourValue;
+  double MinimumLineLength;
+  bool UseRelativeLineLength;
+  QDoubleValidator *ContourValidator;
+  QProgressDialog *Progress;
+  smtk::weak_ptr<smtk::model::Operator> m_edgeOp;
+};
+
+//need a sublcass validator, since QDoubleValidator is really shitty
+class SMTKPQWIDGETSEXT_EXPORT InternalDoubleValidator : public QDoubleValidator
+{
+  Q_OBJECT
+public:
+    InternalDoubleValidator(QObject * parent);
+    virtual void fixup(QString &input) const;
+};
+
+#endif
