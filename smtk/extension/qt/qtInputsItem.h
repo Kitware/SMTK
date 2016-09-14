89f7e93e1befea5c1406090b4cba8dc35aba2305
Robert O'Bara <bob.obara@kitware.com>
2016-09-13 21:10:30 -0400

    ENH: Reorganizing qtInputsItem functionality
    Moved related functionality from qtUIManager to the qtInputsItem class.
    Replaced UIManager's setWidgetColor with specific versions related to
    Invalid, Default or Normal status of the input item.
    
    Closes: #71

--- smtk/extension/qt/qtInputsItem.h
+++ smtk/extension/qt/qtInputsItem.h
@@ -17,14 +17,18 @@
 
 #include "smtk/extension/qt/qtItem.h"
 #include "smtk/extension/qt/Exports.h"
+#include <QDoubleValidator>
 
 class qtInputsItemInternals;
 class QBoxLayout;
+class QLayout;
 
 namespace smtk
 {
   namespace extension
   {
+    class qtUIManager;
+
     class SMTKQTEXT_EXPORT qtInputsItem : public qtItem
     {
       Q_OBJECT
@@ -37,11 +41,17 @@ namespace smtk
 
     public slots:
       void setOutputOptional(int);
+      void onExpressionReferenceChanged();
+      void onTextEditChanged();
+      void onLineEditChanged();
+      void onLineEditFinished();
+      void onInputValueChanged(QObject*);
 
     protected slots:
       virtual void updateItemData();
       virtual void onAddNewValue();
       virtual void onRemoveValue();
+      void displayExpressionWidget(bool checkstate);
 
     protected:
       virtual void createWidget();
@@ -50,13 +60,48 @@ namespace smtk
       virtual void addInputEditor(int i);
       virtual void updateExtensibleState();
       virtual void clearChildWidgets();
+      virtual QWidget* createInputWidget(smtk::attribute::ItemPtr,
+					 int elementIdx, QLayout* childLayout);
+      virtual QWidget* createEditBox(smtk::attribute::ItemPtr,
+				     int elementIdx, QWidget* pWidget);
+      virtual QWidget* createExpressionRefWidget(smtk::attribute::ItemPtr,
+						 int elementIdx);
+
 
     private:
 
       qtInputsItemInternals *Internals;
 
     }; // class
-  }; // namespace attribute
+
+    //A sublcass of QDoubleValidator to fixup input outside of range
+    class SMTKQTEXT_EXPORT qtDoubleValidator : public QDoubleValidator
+    {
+      Q_OBJECT
+    public:
+        qtDoubleValidator(QObject * parent);
+        virtual void fixup(QString &input) const;
+
+        void setUIManager(qtUIManager* uiman);
+    private:
+      qtUIManager* UIManager;
+    };
+
+    //A sublcass of QIntValidator to fixup input outside of range
+    class SMTKQTEXT_EXPORT qtIntValidator : public QIntValidator
+      {
+      Q_OBJECT
+      public:
+        qtIntValidator(QObject * parent);
+        virtual void fixup(QString &input) const;
+
+        void setUIManager(qtUIManager* uiman);
+
+      private:
+        qtUIManager* UIManager;
+      };
+
+  }; // namespace extension
 }; // namespace smtk
 
 #endif
