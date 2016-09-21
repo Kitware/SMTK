1a6fdbb7b01280531980cac5b8c13e21b3041248
John Tourtellott <john.tourtellott@kitware.com>
2016-09-21 15:30:56 -0400

    Add copyright statement to cumulus source files

--- smtk/extension/cumulus/girderrequest.h
+++ smtk/extension/cumulus/girderrequest.h
@@ -1,3 +1,16 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+// .NAME girderrequest.h
+// .SECTION Description
+// .SECTION See Also
+
 #ifndef __smtk_extension_cumulus_girderrequest_h
 #define __smtk_extension_cumulus_girderrequest_h
 
--- smtk/extension/cumulus/logindialog.h
+++ smtk/extension/cumulus/logindialog.h
@@ -1,3 +1,15 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+// .NAME logindialog.h
+// .SECTION Description
+// .SECTION See Also
 
 #ifndef __smtk_extension_cumulus_logindialog_h
 #define __smtk_extension_cumulus_logindialog_h
