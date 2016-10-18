0f0cd6722d7a2d91964d26278de966e516618130
T.J. Corona <tj.corona@kitware.com>
2016-10-18 14:46:42 -0400

    Move mesh session operators to their own subdirectory.

--- smtk/bridge/mesh/operators/ReadOperator.cxx
+++ smtk/bridge/mesh/operators/ReadOperator.cxx
@@ -7,7 +7,7 @@
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //  PURPOSE.  See the above copyright notice for more information.
 //=========================================================================
-#include "smtk/bridge/mesh/ImportOperator.h"
+#include "smtk/bridge/mesh/operators/ReadOperator.h"
 
 #include "smtk/bridge/mesh/Session.h"
 
@@ -17,7 +17,7 @@
 #include "smtk/attribute/ModelEntityItem.h"
 #include "smtk/attribute/StringItem.h"
 
-#include "smtk/io/ImportMesh.h"
+#include "smtk/io/ReadMesh.h"
 
 #include "smtk/model/Group.h"
 #include "smtk/model/Manager.h"
@@ -32,20 +32,16 @@ namespace smtk {
 namespace bridge {
 namespace mesh {
 
-smtk::model::OperatorResult ImportOperator::operateInternal()
+smtk::model::OperatorResult ReadOperator::operateInternal()
 {
   // Get the read file name
   smtk::attribute::FileItem::Ptr filePathItem =
     this->specification()->findFile("filename");
   std::string filePath = filePathItem->value();
 
-  smtk::attribute::StringItem::Ptr labelItem =
-    this->specification()->findString("label");
-  std::string label = labelItem->value();
-
   // Get the collection from the file
   smtk::mesh::CollectionPtr collection =
-    smtk::io::importMesh(filePath, this->activeSession()->meshManager(), label);
+    smtk::io::readMesh(filePath, this->activeSession()->meshManager());
 
   if (!collection || !collection->isValid())
     {
@@ -97,13 +93,13 @@ smtk::model::OperatorResult ImportOperator::operateInternal()
 } //namespace bridge
 } // namespace smtk
 
-#include "smtk/bridge/mesh/ImportOperator_xml.h"
+#include "smtk/bridge/mesh/ReadOperator_xml.h"
 #include "smtk/bridge/mesh/Exports.h"
 
 smtkImplementsModelOperator(
   SMTKMESHSESSION_EXPORT,
-  smtk::bridge::mesh::ImportOperator,
-  mesh_import,
-  "import",
-  ImportOperator_xml,
+  smtk::bridge::mesh::ReadOperator,
+  mesh_read,
+  "read",
+  ReadOperator_xml,
   smtk::bridge::mesh::Session);
