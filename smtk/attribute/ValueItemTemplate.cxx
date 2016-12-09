438ade3a1c2e3b4a02a8d1839451947dd514b512
T.J. Corona <tj.corona@kitware.com>
2016-12-09 11:34:39 -0500

    Add implementations for ValueItem*Template used for pybind11 generation.

---
+++ smtk/attribute/ValueItemTemplate.cxx
@@ -0,0 +1,26 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+
+
+#include "smtk/attribute/ValueItemTemplate.h"
+
+namespace smtk
+{
+  namespace attribute
+  {
+    // To auto-generate pybind11 bindings, we read out class information using
+    // pygccxml, which only reports template instances. We therefore explicitly
+    // declare the three used instances of ValueItemTemplate to facilitate the
+    // scripted generation of pybind11 bindings.
+    template class ValueItemTemplate<int>;
+    template class ValueItemTemplate<double>;
+    template class ValueItemTemplate<std::string>;
+  }
+}
