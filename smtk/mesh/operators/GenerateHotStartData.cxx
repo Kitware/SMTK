ab9ee17b971e809614cf3aa2f4bb3f586c695593
T.J. Corona <tj.corona@kitware.com>
2017-06-21 14:21:12 -0500

    Add "AdH - Generate Hot Start Data" operator.

---
+++ smtk/mesh/operators/GenerateHotStartData.cxx
@@ -0,0 +1,230 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+
+#include "smtk/mesh/operators/GenerateHotStartData.h"
+
+#include "smtk/attribute/Attribute.h"
+#include "smtk/attribute/DoubleItem.h"
+#include "smtk/attribute/FileItem.h"
+#include "smtk/attribute/GroupItem.h"
+#include "smtk/attribute/IntItem.h"
+#include "smtk/attribute/MeshItem.h"
+#include "smtk/attribute/StringItem.h"
+
+#include "smtk/mesh/ApplyToMesh.h"
+#include "smtk/mesh/CellField.h"
+#include "smtk/mesh/MeshSet.h"
+#include "smtk/mesh/PointField.h"
+
+#include "smtk/mesh/interpolation/InverseDistanceWeighting.h"
+#include "smtk/mesh/interpolation/PointCloud.h"
+
+#include "smtk/model/Manager.h"
+#include "smtk/model/Session.h"
+
+#include <array>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// We use either STL regex or Boost regex, depending on support. These flags
+// correspond to the equivalent logic used to determine the inclusion of Boost's
+// regex library.
+#if defined(SMTK_CLANG) ||                                                                         \
+  (defined(SMTK_GCC) && __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)) ||                 \
+  defined(SMTK_MSVC)
+#include <regex>
+using std::regex;
+using std::sregex_token_iterator;
+using std::regex_replace;
+using std::regex_search;
+using std::regex_match;
+#else
+#include <boost/regex.hpp>
+using boost::regex;
+using boost::sregex_token_iterator;
+using boost::regex_replace;
+using boost::regex_search;
+using boost::regex_match;
+#endif
+
+namespace
+{
+// A key that corresponds to the .sbt file's values for output field type.
+enum
+{
+  CELL_FIELD = 0,
+  POINT_FIELD = 1
+};
+
+bool readCSVFile(
+  const std::string& fileName, std::vector<double>& coordinates, std::vector<double>& values)
+{
+  std::ifstream infile(fileName.c_str());
+  if (!infile.good())
+  {
+    return false;
+  }
+  std::string line;
+  regex re(",");
+  while (std::getline(infile, line))
+  {
+    // passing -1 as the submatch index parameter performs splitting
+    sregex_token_iterator first{ line.begin(), line.end(), re, -1 }, last;
+
+    // Se are looking for (x, y, value). So, we must have at least 3
+    // components.
+    if (std::distance(first, last) < 3)
+    {
+      return false;
+    }
+
+    coordinates.push_back(std::stod(*(first++)));
+    coordinates.push_back(std::stod(*(first++)));
+    coordinates.push_back(0.);
+    values.push_back(std::stod(*(first++)));
+  }
+
+  infile.close();
+  return true;
+}
+}
+
+namespace smtk
+{
+namespace mesh
+{
+
+bool GenerateHotStartData::ableToOperate()
+{
+  if (!this->ensureSpecification())
+  {
+    return false;
+  }
+
+  smtk::attribute::MeshItem::Ptr meshItem = this->specification()->findMesh("mesh");
+  if (!meshItem || meshItem->numberOfValues() == 0)
+  {
+    return false;
+  }
+
+  return true;
+}
+
+smtk::model::OperatorResult GenerateHotStartData::operateInternal()
+{
+  // Access the mesh to elevate
+  smtk::attribute::MeshItem::Ptr meshItem = this->specification()->findMesh("mesh");
+
+  // Access the data set type
+  smtk::attribute::StringItem::Ptr typeItem = this->specification()->findString("dstype");
+
+  // Access the interpolation points
+  smtk::attribute::GroupItem::Ptr interpolationPointsItem = this->findGroup("points");
+
+  // Access the interpolation power parameter
+  smtk::attribute::DoubleItem::Ptr powerItem = this->findDouble("power");
+
+  // Construct containers for our source points
+  std::vector<double> sourceCoordinates;
+  std::vector<double> sourceValues;
+
+  // Access the points CSV file name, if it is enabled
+  smtk::attribute::FileItem::Ptr ptsFileItem = this->specification()->findFile("ptsfile");
+  if (ptsFileItem->isEnabled())
+  {
+    bool success = readCSVFile(ptsFileItem->value(0), sourceCoordinates, sourceValues);
+    if (!success)
+    {
+      smtkErrorMacro(this->log(), "Could not read CSV file.");
+      return this->createResult(smtk::model::OPERATION_FAILED);
+    }
+  }
+
+  for (std::size_t i = 0; i < interpolationPointsItem->numberOfGroups(); i++)
+  {
+    smtk::attribute::DoubleItemPtr pointItem =
+      smtk::dynamic_pointer_cast<smtk::attribute::DoubleItem>(interpolationPointsItem->item(i, 0));
+    for (std::size_t j = 0; j < 2; j++)
+    {
+      sourceCoordinates.push_back(pointItem->value(j));
+    }
+    sourceCoordinates.push_back(0.);
+    sourceValues.push_back(pointItem->value(2));
+  }
+
+  // Construct an instance of our interpolator and set its parameters
+  smtk::mesh::PointCloud pointcloud(sourceCoordinates, sourceValues);
+  smtk::mesh::InverseDistanceWeighting interpolator(pointcloud, powerItem->value());
+
+  // Access the attribute associated with the modified meshes
+  smtk::model::OperatorResult result = this->createResult(smtk::model::OPERATION_SUCCEEDED);
+  smtk::attribute::MeshItem::Ptr modifiedMeshes = result->findMesh("mesh_modified");
+  modifiedMeshes->setNumberOfValues(meshItem->numberOfValues());
+
+  // Access the attribute associated with the changed tessellation
+  smtk::attribute::ModelEntityItem::Ptr modifiedEntities = result->findModelEntity("tess_changed");
+  modifiedEntities->setNumberOfValues(meshItem->numberOfValues());
+
+  std::function<double(std::array<double, 3>)> fn;
+  std::string name;
+
+  if (typeItem->value() == "Initial Depth")
+  {
+    name = "ioh";
+    fn = [&](std::array<double, 3> x) { return interpolator(x); };
+  }
+  else if (typeItem->value() == "Initial Concentration")
+  {
+    name = "icon 1";
+    fn = [&](std::array<double, 3> x) { return interpolator(x); };
+  }
+  else if (typeItem->value() == "Initial Water Surface Elevation")
+  {
+    name = "ioh";
+    fn = [&](std::array<double, 3> x) { return interpolator(x) - x[2]; };
+  }
+  else
+  {
+    smtkErrorMacro(this->log(), "Unsupported data type.");
+    return this->createResult(smtk::model::OPERATION_FAILED);
+  }
+
+  // apply the interpolator to the meshes and populate the result attributes
+  for (std::size_t i = 0; i < meshItem->numberOfValues(); i++)
+  {
+    smtk::mesh::MeshSet mesh = meshItem->value(i);
+
+    smtk::mesh::applyScalarPointField(fn, name, mesh);
+
+    modifiedMeshes->appendValue(mesh);
+
+    smtk::model::EntityRefArray entities;
+    bool entitiesAreValid = mesh.modelEntities(entities);
+    if (entitiesAreValid && !entities.empty())
+    {
+      smtk::model::Model model = entities[0].owningModel();
+      this->addEntityToResult(result, model, MODIFIED);
+      modifiedEntities->appendValue(model);
+    }
+  }
+
+  return result;
+}
+}
+}
+
+#include "smtk/mesh/GenerateHotStartData_xml.h"
+
+smtkImplementsModelOperator(SMTKCORE_EXPORT, smtk::mesh::GenerateHotStartData,
+  generate_hot_start_data, "generate hot start data", GenerateHotStartData_xml,
+  smtk::model::Session);
---
+++ smtk/mesh/operators/GenerateHotStartData.sbt
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<!-- Description of the "gnerate hot start data" Operator -->
+<SMTK_AttributeSystem Version="2">
+  <Definitions>
+    <!-- Operator -->
+    <AttDef Type="generate hot start data"
+            Label="AdH - Generate Hot Start Data" BaseType="operator">
+      <BriefDescription>
+        Create a field on mesh nodes/elements from
+        interpolated 2-dimensional data.
+      </BriefDescription>
+      <DetailedDescription>
+        &lt;p&gt;Create a field on mesh nodes/elements from
+        interpolated 2-dimensional data.
+        &lt;p&gt;This operator accepts as input two-dimensional points
+        with associated scalar values, and interpolates these values
+        onto either the points or the cells of the mesh. The input
+        points can be inserted manually or read from a CSV file.
+      </DetailedDescription>
+      <ItemDefinitions>
+        <MeshEntity Name="mesh" Label="Mesh" NumberOfRequiredValues="1" Extensible="true" >
+          <BriefDescription>The mesh to elevate.</BriefDescription>
+        </MeshEntity>
+
+        <String Name="dstype" Label="Data Type" NumberOfRequiredValues="1">
+          <BriefDescription>The name of the data set.</BriefDescription>
+          <DiscreteInfo DefaultIndex="0">
+            <Value Enum="Initial Depth">Initial Depth</Value>
+            <Value Enum="Initial Concentration">Initial Concentration</Value>
+            <Value Enum="Initial Water Surface Elevation">Initial
+            Water Surface Elevation</Value>
+          </DiscreteInfo>
+        </String>
+
+        <File Name="ptsfile" Label="Input CSV File" NumberOfValues="1"
+              ShouldExist="true" Optional="true" FileFilters="CSV file (*.csv)">
+        </File>
+
+        <Group Name="points" Label="Interpolation Points"
+               Extensible="true" NumberOfRequiredGroups="0" >
+          <BriefDescription>The points and their scalar values to interpolate.</BriefDescription>
+          <DetailedDescription>
+            An implicit function is described by interpolating points
+            and their associated scalar value.
+          </DetailedDescription>
+          <ItemDefinitions>
+            <Double Name="point" Label="Point" NumberOfRequiredValues="3">
+              <ComponentLabels>
+                <Label>X</Label>
+                <Label>Y</Label>
+                <Label>Scalar</Label>
+              </ComponentLabels>
+            </Double>
+          </ItemDefinitions>
+        </Group>
+
+        <Double Name="power" NumberOfRequiredValues="1" Extensible="no">
+          <BriefDescription>The weighting power used to interpolate
+          source points.</BriefDescription>
+          <DetailedDescription>
+            The weighting power used to interpolate source points.
+
+            The operator uses Shepard's method to interpolate between
+            points. The inverse distance value is exponentiated by
+            this unitless value when computing weighting coefficients.
+          </DetailedDescription>
+          <DefaultValue>1.</DefaultValue>
+        </Double>
+      </ItemDefinitions>
+    </AttDef>
+    <!-- Result -->
+    <AttDef Type="result(generate hot start data)" BaseType="result">
+      <ItemDefinitions>
+        <MeshEntity Name="mesh_modified" NumberOfRequiredValues="0" Extensible="true" AdvanceLevel="11"/>
+        <ModelEntity Name="tess_changed" NumberOfRequiredValues="0"
+                     Extensible="true" AdvanceLevel="11"/>
+      </ItemDefinitions>
+    </AttDef>
+  </Definitions>
+</SMTK_AttributeSystem>
