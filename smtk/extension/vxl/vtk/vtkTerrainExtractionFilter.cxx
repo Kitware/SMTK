5873b4639a84cf18fa59d2a73e54066ce6009278
Haocheng Liu <haocheng.liu@kitware.com>
2017-07-28 10:40:29 -0400

    Move rtvl into smtk thirdparty

--- smtk/extension/vxl/vtk/vtkTerrainExtractionFilter.cxx
+++ smtk/extension/vxl/vtk/vtkTerrainExtractionFilter.cxx
@@ -34,8 +34,8 @@
 
 // refine
 #include <rgtl/rgtl_serialize_ostream.hxx>
-#include <smtk/extension/vxl/rtvl/rtvl_refine.hxx>
-#include <smtk/extension/vxl/rtvl/rtvl_tokens.hxx>
+#include <thirdparty/rtvl/rtvl_refine.hxx>
+#include <thirdparty/rtvl/rtvl_tokens.hxx>
 
 #include <vcl_fstream.h>
 #include <vcl_memory.h>
@@ -43,13 +43,13 @@
 
 // extract
 #include <rgtl/rgtl_serialize_istream.hxx>
-#include <smtk/extension/vxl/rtvl/rtvl_tensor.hxx>
-#include <smtk/extension/vxl/rtvl/rtvl_tensor_d.hxx>
-#include <smtk/extension/vxl/rtvl/rtvl_vote.hxx>
-#include <smtk/extension/vxl/rtvl/rtvl_votee.hxx>
-#include <smtk/extension/vxl/rtvl/rtvl_votee_d.hxx>
-#include <smtk/extension/vxl/rtvl/rtvl_voter.hxx>
-#include <smtk/extension/vxl/rtvl/rtvl_weight_smooth.hxx>
+#include <thirdparty/rtvl/rtvl_tensor.hxx>
+#include <thirdparty/rtvl/rtvl_tensor_d.hxx>
+#include <thirdparty/rtvl/rtvl_vote.hxx>
+#include <thirdparty/rtvl/rtvl_votee.hxx>
+#include <thirdparty/rtvl/rtvl_votee_d.hxx>
+#include <thirdparty/rtvl/rtvl_voter.hxx>
+#include <thirdparty/rtvl/rtvl_weight_smooth.hxx>
 
 #include <rgtl/rgtl_object_array_points.hxx>
 #include <rgtl/rgtl_octree_cell_bounds.hxx>
