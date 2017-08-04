0f86dbedc8f4bcaf684a9014ec379b517738c479
Haocheng Liu <haocheng.liu@kitware.com>
2017-08-04 14:27:03 -0400

    BUG: Fix vxl building issue on windows
    Add missing libs and install rules.

--- smtk/extension/vxl/vtk/module.cmake
+++ smtk/extension/vxl/vtk/module.cmake
@@ -1,10 +1,19 @@
 set (__dependencies)
-list (APPEND __dependencies vtksys)
+list (APPEND __dependencies
+      vtkCommonDataModel
+      vtkCommonSystem
+      vtkCommonTransforms
+      vtkIOXML
+      vtksys
+      vtkFiltersCore
+      vtkFiltersGeneral
+      )
 
 if (SMTK_ENABLE_VXL_SUPPORT)
   vtk_module(vtkSMTKVXLExt
     DEPENDS
       vtkCommonCore
+      vtkCommonExecutionModel
     PRIVATE_DEPENDS
       ${__dependencies}
     TEST_DEPENDS
