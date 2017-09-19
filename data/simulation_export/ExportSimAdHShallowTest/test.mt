13e7fdda1a9159932da553a01d1effaaf9c6c486
Ben Boeckel <ben.boeckel@kitware.com>
2017-09-19 09:28:20 -0400

    data: add export simulation data for CMB

---
+++ data/simulation_export/ExportSimAdHShallowTest/test.mt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f11ad6bbc7ecca0b2416b713dee77f1a635c00aaeaa946e14cde1c2bfae56d5
+size 4
---
+++ data/simulation_export/ExportSimProteusTest/test_p.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:323cea02bcdd2dcdf0c018042575aabbd78cc67cd7d7e8e158e3d7c0619fc423
+size 839
