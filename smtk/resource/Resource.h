27c7120d83d777b0a29aac34324fac9de8006ecc
T.J. Corona <tj.corona@kitware.com>
2018-03-14 23:30:23 -0400

    Resource.h: fix comments.

--- smtk/resource/Resource.h
+++ smtk/resource/Resource.h
@@ -50,10 +50,7 @@ class Metadata;
   static constexpr const char* const type_name = Name;                                             \
   virtual std::string uniqueName() const override { return type_name; }
 
-/**\brief An abstract base class for SMTK resources.
-  *
-  * Resources are attributes, models, or meshes.
-  */
+/// An abstract base class for SMTK resources.
 class SMTKCORE_EXPORT Resource : smtkEnableSharedPtr(Resource)
 {
 public:
@@ -68,67 +65,66 @@ public:
 
   virtual std::string uniqueName() const = 0;
 
-  // index is a compile-time intrinsic of the derived resource; as such, it
-  // cannot be set.
+  /// index is a compile-time intrinsic of the derived resource; as such, it
+  /// cannot be set.
   virtual Index index() const { return std::type_index(typeid(*this)).hash_code(); }
 
-  // given a resource type, return whether or not this resource is or is
-  // derived from the resource described by the index.
+  /// given a resource type, return whether or not this resource is or is
+  /// derived from the resource described by the index.
   template <class ResourceType>
   bool isOfType() const
   {
     return this->isOfType(std::type_index(typeid(ResourceType)).hash_code());
   }
 
-  // given a resource index, return whether or not this resource is or is
-  // derived from the resource described by the index.
+  /// given a resource index, return whether or not this resource is or is
+  /// derived from the resource described by the index.
   bool isOfType(const Index& index) const;
 
-  // given a resource's unique name, return whether or not this resource is or
-  // is derived from the resource described by the name.
+  /// given a resource's unique name, return whether or not this resource is or
+  /// is derived from the resource described by the name.
   bool isOfType(const std::string& uniqueName) const;
 
-  // id and location are run-time intrinsics of the derived resource; we need to
-  // allow the user to reset these values.
+  /// id and location are run-time intrinsics of the derived resource; we need
+  /// to allow the user to reset these values.
   const smtk::common::UUID& id() const { return this->m_id; }
   const std::string& location() const { return this->m_location; }
 
   bool setId(const smtk::common::UUID& myID);
   bool setLocation(const std::string& location);
 
-  /**\brief Indicate whether the resource is in sync with its location.
-    *
-    * Resources that have a non-empty location and are identical to
-    * the data stored at location are clean. All other resources are dirty.
-    *
-    * Operations that are write-operators (i.e., not read-only) should mark
-    * resources as modified. Saving a resource using its metadata's write
-    * method will mark the resource as clean. Loading a resource using
-    * its metadata's read method should return a clean resource.
-    */
+  /// Indicate whether the resource is in sync with its location.
+  ///
+  /// Resources that have a non-empty location and are identical to
+  /// the data stored at location are clean. All other resources are dirty.
+  ///
+  /// Operations that are write-operators (i.e., not read-only) should mark
+  /// resources as modified. Saving a resource using its metadata's write
+  /// method will mark the resource as clean. Loading a resource using
+  /// its metadata's read method should return a clean resource.
   bool clean() const { return m_clean; }
   void setClean(bool state = true) { m_clean = state; }
 
-  // Resources that are managed have a non-null pointer to their manager.
+  /// Resources that are managed have a non-null pointer to their manager.
   ManagerPtr manager() const { return this->m_manager.lock(); }
 
-  // given a resource component's UUID, return the resource component.
+  /// given a resource component's UUID, return the resource component.
   virtual ComponentPtr find(const smtk::common::UUID& compId) const = 0;
 
-  // given a std::string describing a query, return a functor for performing the
-  // query (accepts component as input, returns true if the component satisfies
-  // the query parameters).
+  /// given a std::string describing a query, return a functor for performing
+  /// the query (accepts component as input, returns true if the component
+  /// satisfies the query parameters).
   virtual std::function<bool(const ComponentPtr&)> queryOperation(const std::string&) const = 0;
 
-  // visit all components in a resource.
+  /// visit all components in a resource.
   virtual void visit(std::function<void(const ComponentPtr&)>& v) const = 0;
 
-  // given a a std::string describing a query, return a set of components that
-  // satisfy the query criteria.
+  /// given a a std::string describing a query, return a set of components that
+  /// satisfy the query criteria.
   ComponentSet find(const std::string& queryString) const;
 
-  // classes that are granted permission to the key may retrieve the resource's
-  // lock.
+  /// classes that are granted permission to the key may retrieve the resource's
+  /// lock.
   Lock& lock(Key()) const { return this->m_lock; }
 
 protected:
@@ -138,7 +134,8 @@ protected:
 private:
   smtk::common::UUID m_id;
   std::string m_location;
-  bool m_clean; //!< True when m_location is in sync with this instance; false otherwise.
+  /// True when m_location is in sync with this instance.
+  bool m_clean;
 
   mutable Lock m_lock;
 
