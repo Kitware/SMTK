26febca08eac2ee68af9f0492e4acd6284cd70f4
Robert O'Bara <bob.obara@kitware.com>
2018-09-25 15:08:49 -0400

    ENH: Changing Icons for Model and Attribute
    Added new icons for model and attribute resources as well as an icon for attributes.

--- smtk/attribute/testing/python/associationTest.py
+++ smtk/attribute/testing/python/associationTest.py
@@ -33,9 +33,44 @@ if __name__ == '__main__':
     defn = resource.createDefinition('testdef')
     resource.setRefModelResource(mmgr)
     defn.setLocalAssociationMask(int(mask))
+    defn.associationRule().setIsExtensible(True)
 
     # Create attribute and associate to group item
     att = resource.createAttribute('testatt', defn)
-    att.associateEntity(group_item.entity())
+    a = att.associateEntity(group_item.entity())
+
+    if a:
+        print "Association Worked"
+    else:
+        print "Association Failed"
+        status = -1
+
+    uuids = att.associatedModelEntityIds()
+    print "the number of ents associated with the att is ", len(uuids)
+
+    # Check to see if the attribute is associated with the entity
+    if att.isEntityAssociated(group_item):
+        print "Attribute knows about the entity via UUID"
+    else:
+        print "Attribute does not know about the entity via UUID"
+        status = -1
+
+    # Check to see if the model entity knows about the attribute
+    if group_item.hasAttributes():
+        print "Model Entity does have attributes"
+        if group_item.hasAttribute(att.id()):
+            print "Model Entity has this attribute associated"
+        else:
+            print "Model Entity does not have this attribute associated with it"
+            status = -1
+        uuids = group_item.attributes()
+        print "the number of attributes associated with the ent is ", len(uuids)
+        # There should be only 1 attribute on it
+        if len(uuids) == 1:
+            print "Model Entity return correct attribute list size"
+        if att.id() in uuids:
+            print "Model Entity did return the attribute via attributes() call"
+    else:
+        print "Model Entity does not have any attributes"
 
     sys.exit(status)
