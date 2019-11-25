765f4852d70102e774b4a4855414c1041cbde409
Ben Boeckel <ben.boeckel@kitware.com>
2019-11-25 10:36:45 -0500

    gitlab-ci: add a Python2 builder

---
+++ .gitlab/ci/docker/fedora31-vtk-python2/install_deps.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Install build requirements.
+dnf install -y \
+    zlib-devel libcurl-devel python-devel \
+    freeglut-devel glew-devel graphviz-devel libpng-devel \
+    libxcb libxcb-devel libXt-devel xcb-util xcb-util-devel mesa-libGL-devel \
+    libxkbcommon-devel diffutils hostname file
+
+# Install development tools
+dnf install -y \
+    gcc-c++ \
+    qt5-qtbase-devel \
+    qt5-qttools-devel \
+    qt5-qtx11extras-devel \
+    qt5-qtxmlpatterns-devel \
+    cmake \
+    git-core \
+    ninja-build \
+    make
+
+dnf clean all
