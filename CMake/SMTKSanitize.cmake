c2645bdc5354e228a9343d0f1d1ed8218fae9719
Ben Boeckel <ben.boeckel@kitware.com>
2019-12-04 12:02:34 -0500

    cmake: add support for running sanitizers

---
+++ CMake/SMTKSanitize.cmake
@@ -0,0 +1,34 @@
+#=========================================================================
+#
+#  This software is distributed WITHOUT ANY WARRANTY; without even
+#  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.  See the above copyright notice for more information.
+#
+#=========================================================================
+
+# This code has been adapted from remus (https://gitlab.kitware.com/cmb/remus)
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR
+   CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+  set(CMAKE_COMPILER_IS_CLANGXX 1)
+endif()
+
+if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX)
+
+  #Add option for enabling sanitizers
+  option(SMTK_ENABLE_SANITIZER "Build with sanitizer support." OFF)
+  mark_as_advanced(SMTK_ENABLE_SANITIZER)
+
+  if(SMTK_ENABLE_SANITIZER)
+    set(SMTK_SANITIZER "address"
+      CACHE STRING "The sanitizer to use")
+    mark_as_advanced(SMTK_SANITIZER)
+
+    #We're setting the CXX flags and C flags beacuse they're propagated down
+    #independent of build type.
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=${SMTK_SANITIZER}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=${SMTK_SANITIZER}")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=${SMTK_SANITIZER}")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=${SMTK_SANITIZER}")
+  endif()
+endif()
