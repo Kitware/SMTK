18422bfbbefe7f79fb71fc5f6b2662d4e4bd787f
Ben Boeckel <ben.boeckel@kitware.com>
2020-01-24 14:15:46 -0500

    ci: contract test RGG and project-manager

--- .gitlab/ci/configure_fedora31_paraview.cmake
+++ .gitlab/ci/configure_fedora31_paraview.cmake
@@ -1,1 +1,6 @@
+set(SMTK_PLUGIN_CONTRACT_FILE_URLS
+  "https://gitlab.kitware.com/cmb/plugins/rgg-session/raw/master/CMake/rgg-session.cmake"
+  "https://gitlab.kitware.com/cmb/plugins/project-manager/raw/master/CMake/project-manager.cmake"
+  CACHE STRING "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora31.cmake")
