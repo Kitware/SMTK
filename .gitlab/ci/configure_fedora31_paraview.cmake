fad2596d739cb02dc1cb45a56eb5e0a5a19872a0
John Tourtellott <john.tourtellott@kitware.com>
2020-02-19 14:34:34 -0500

    Add truchas-extensions to the contract test list

--- .gitlab/ci/configure_fedora31_paraview.cmake
+++ .gitlab/ci/configure_fedora31_paraview.cmake
@@ -1,6 +1,7 @@
 set(SMTK_PLUGIN_CONTRACT_FILE_URLS
   "https://gitlab.kitware.com/cmb/plugins/rgg-session/raw/master/CMake/rgg-session.cmake"
   "https://gitlab.kitware.com/cmb/plugins/project-manager/raw/master/CMake/project-manager.cmake"
+  "https://gitlab.kitware.com/cmb/plugins/truchas-extensions/raw/master/CMake/truchas-extensions.cmake"
   CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora31.cmake")
