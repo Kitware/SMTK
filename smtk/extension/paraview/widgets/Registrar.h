0629029d3684d4e1f82708d97d07f85b1d460366
T.J. Corona <tj.corona@kitware.com>
2020-03-17 15:52:17 -0400

    Add qt registrar as dependency for widgets registrar

--- smtk/extension/paraview/widgets/Registrar.h
+++ smtk/extension/paraview/widgets/Registrar.h
@@ -14,6 +14,8 @@
 #include "smtk/extension/paraview/widgets/smtkPQWidgetsExtModule.h"
 #include "smtk/view/Manager.h"
 
+#include "smtk/extension/qt/qtViewRegistrar.h"
+
 namespace smtk
 {
 namespace extension
@@ -26,6 +28,8 @@ namespace widgets
 class SMTKPQWIDGETSEXT_EXPORT Registrar
 {
 public:
+  using Dependencies = std::tuple<qtViewRegistrar>;
+
   static void registerTo(const smtk::view::Manager::Ptr&);
   static void unregisterFrom(const smtk::view::Manager::Ptr&);
 };
