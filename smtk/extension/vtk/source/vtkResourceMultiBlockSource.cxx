d768ca323d40225a9d1a58e11f177a83e4de3d0c
David Thompson <david.thompson@kitware.com>
2020-09-12 12:46:33 -0400

    Properly initialize the LastModified ivar.

--- smtk/extension/vtk/source/vtkResourceMultiBlockSource.cxx
+++ smtk/extension/vtk/source/vtkResourceMultiBlockSource.cxx
@@ -31,6 +31,7 @@ vtkInformationKeyMacro(vtkResourceMultiBlockSource, COMPONENT_ID, String);
 
 //----------------------------------------------------------------------------
 vtkResourceMultiBlockSource::vtkResourceMultiBlockSource()
+  : LastModified(0)
 {
   this->SetNumberOfInputPorts(0);
 }
