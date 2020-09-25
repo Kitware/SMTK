a22e27089eb8563d50c4c92aabb19abb571a1ccb
John Tourtellott <john.tourtellott@kitware.com>
2020-09-25 17:24:15 -0400

    Add contract test for ace3p-extensions

--- .gitlab/ci/configure_fedora31_paraview.cmake
+++ .gitlab/ci/configure_fedora31_paraview.cmake
@@ -4,6 +4,7 @@ set(SMTK_PLUGIN_CONTRACT_FILE_URLS
   "https://gitlab.kitware.com/cmb/plugins/project-manager/-/raw/master/CMake/project-manager.cmake"
   "https://gitlab.kitware.com/cmb/plugins/truchas-extensions/-/raw/master/CMake/truchas-extensions.cmake"
   "https://gitlab.kitware.com/cmb/cmb/-/raw/master/cmake/cmb.cmake"
+  "https://gitlab.kitware.com/cmb/plugins/ace3p-extensions/-/raw/master/CMake/ace3p-extensions.cmake"
   CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora31.cmake")
