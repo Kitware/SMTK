8c6b6e870ac2656af2251836082516e576a0b59f
Aron Helser <aron.helser@kitware.com>
2020-11-18 15:36:16 -0500

    Fix expandXMLString to correctly handle includes of includes
    The recursive call was only returning the final include file content
    not the intermediate file's content. Switch to non-recursive loop
    instead.

--- CMake/EncodeStringFunctions.cmake
+++ CMake/EncodeStringFunctions.cmake
@@ -1,61 +1,62 @@
 # Given a string representing an XML file, extract all of the include items and
 # replace them with the contents of the file being included.
-function(expandXMLString rawString expandedString includedFiles includeDirectories)
+function(expandXMLString rawString expandedString includeDirectories)
 
+  set(includedFiles)
   # First, grab all of the xml elements describing included files
   string(REGEX MATCHALL "<include[^/]* href=\"[^\"]+\"[^/]*/>" includes ${${rawString}})
 
-  # For each xml element...
-  foreach (include IN LISTS includes)
+  while (includes)
+    # For each xml element...
+    foreach (include IN LISTS includes)
 
-    # ...extract the attribute with the include file name...
-    string(REGEX MATCH "href=\"[^\"]+\"" tmp ${include})
+      # ...extract the attribute with the include file name...
+      string(REGEX MATCH "href=\"[^\"]+\"" tmp ${include})
 
-    # ...and extract the substring that contains the bare file name.
-    string(LENGTH ${tmp} len)
-    math(EXPR newlen "${len} - 7")
-    string(SUBSTRING ${tmp} 6 ${newlen} incFileName)
+      # ...and extract the substring that contains the bare file name.
+      string(LENGTH ${tmp} len)
+      math(EXPR newlen "${len} - 7")
+      string(SUBSTRING ${tmp} 6 ${newlen} incFileName)
 
-    # Locate the included file so we can expand it. Check the list of include
-    # directories, but do not use find_file() as that caches results.
-    foreach (includeDir IN LISTS includeDirectories)
-      string(CONCAT absoluteIncFileName ${includeDir} "/" ${incFileName})
-      if (EXISTS ${absoluteIncFileName})
-        set(incFileName ${absoluteIncFileName})
-        break()
+      # Locate the included file so we can expand it. Check the list of include
+      # directories, but do not use find_file() as that caches results.
+      foreach (includeDir IN LISTS includeDirectories)
+        string(CONCAT absoluteIncFileName ${includeDir} "/" ${incFileName})
+        if (EXISTS ${absoluteIncFileName})
+          set(incFileName ${absoluteIncFileName})
+          break()
+        endif()
+      endforeach()
+      find_file(incFileName NAMES ${incFileName} PATHS ${includeDirectories} NO_DEFAULT_PATH)
+
+      list(FIND includedFiles ${incFileName} index)
+
+      if (NOT ${index} EQUAL -1)
+        message(WARNING "File \"${incFileName}\" has cyclic dependencies.")
+        continue()
+      endif ()
+
+      # If we cannot locate the include file, leave the include directive in the
+      # xml description.
+      if (NOT EXISTS ${incFileName})
+        message(WARNING "Cannot locate file \"${incFileName}\".")
+        continue()
       endif()
+
+      # Read the dependency file
+      file(READ ${incFileName} incFileContents)
+
+      # Add the file name to the list of included files
+      list(APPEND includedFiles ${incFileName})
+
+      # Replace the include element with the contents of the included file.
+      string(REPLACE ${include} ${incFileContents} ${rawString} "${${rawString}}")
     endforeach()
-    find_file(incFileName NAMES ${incFileName} PATHS ${includeDirectories} NO_DEFAULT_PATH)
 
-    list(FIND includedFiles ${incFileName} index)
+    # search for more includes, from the new content.
+    string(REGEX MATCHALL "<include[^/]* href=\"[^\"]+\"[^/]*/>" includes ${${rawString}})
 
-    if (NOT ${index} EQUAL -1)
-      message(WARNING "File \"${incFileName}\" has cyclic dependencies.")
-      continue()
-    endif ()
-
-    # If we cannot locate the include file, leave the include directive in the
-    # xml description.
-    if (NOT EXISTS ${incFileName})
-      message(WARNING "Cannot locate file \"${incFileName}\".")
-      continue()
-    endif()
-
-    # Read the dependency file
-    file(READ ${incFileName} incFileContents)
-
-    # Add the file name to the list of included files
-    list(APPEND includedFiles ${incFileName})
-
-    # Recursively expand the dependency xml string
-    expandXMLString(incFileContents expandedIncFileContents includedFiles includeDirectories)
-
-    # Pop the file name
-    list(REMOVE_ITEM includedFiles ${incFileName})
-
-    # Replace the include element with the contents of the included file.
-    string(REPLACE ${include} ${expandedIncFileContents} ${rawString} "${${rawString}}")
-  endforeach()
+  endwhile()
 
   set(${expandedString} "${${rawString}}" PARENT_SCOPE)
 
@@ -63,8 +64,7 @@ endfunction()
 
 function(encodeStringAsCVariable rawString encodedVarName stringOut includeDirectories)
 
-  set(includedFiles)
-  expandXMLString(${rawString} expandedString includedFiles "${includeDirectories}")
+  expandXMLString(${rawString} expandedString "${includeDirectories}")
 
   string(REPLACE "\\" "\\\\" str1 "${expandedString}")
   string(REPLACE "\"" "\\\"" str2 "${str1}")
@@ -102,7 +102,7 @@ endfunction()
 # Example:
 #   configureFileAsCVariable("foo.xml" "bar.cxx" "operatorSpec")
 # would read "foo.xml", escape it as a C string, and write the
-# assigment:
+# assignment:
 #   static const char operatorSpec[] = "...";
 # into bar.cxx (where "..." is the encoded contents of "foo.xml").
 #
@@ -117,8 +117,7 @@ endfunction()
 
 function(encodeStringAsPyVariable rawString encodedVarName stringOut includeDirectories)
 
-  set(includedFiles)
-  expandXMLString(${rawString} expandedString includedFiles "${includeDirectories}")
+  expandXMLString(${rawString} expandedString "${includeDirectories}")
 
   string(CONFIGURE "\ndescription = '''\n${expandedString}\n'''\n" pyString)
   set(${stringOut} "${pyString}" PARENT_SCOPE)
@@ -150,7 +149,7 @@ endfunction()
 
 # Example:
 #   configureFileAsPyVariable("foo.xml" "bar.py" "operatorSpec")
-# would read "foo.xml" and write the assigment:
+# would read "foo.xml" and write the assignment:
 #   operatorSpec = '''
 #   ...
 #   '''
