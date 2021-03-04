c52a2d757205b54801a327dd97b6d5e2175de477
Ben Boeckel <ben.boeckel@kitware.com>
2021-03-04 10:47:04 -0500

    ci: build Qt for the ParaView image
    The sqlite from the superbuild conflicts with the sqlite plugin used in
    Fedora's Qt.

--- .gitlab/ci/docker/fedora31-paraview/install_deps.sh
+++ .gitlab/ci/docker/fedora31-paraview/install_deps.sh
@@ -2,7 +2,7 @@
 
 # Install build requirements.
 dnf install -y \
-    zlib-devel libcurl-devel python-devel \
+    zlib-devel libcurl-devel python-devel python-unversioned-command \
     freeglut-devel glew-devel graphviz-devel libpng-devel \
     libxcb libxcb-devel libXt-devel xcb-util xcb-util-devel mesa-libGL-devel \
     libxkbcommon-devel diffutils hostname file
@@ -10,17 +10,13 @@ dnf install -y \
 # Install development tools
 dnf install -y \
     gcc-c++ \
-    qt5-qtbase-devel \
-    qt5-qtsvg-devel \
-    qt5-qttools-devel \
-    qt5-qtx11extras-devel \
-    qt5-qtxmlpatterns-devel \
     cmake \
     git-core \
     git-lfs \
     ninja-build \
     make \
-    chrpath
+    chrpath \
+    which
 
 # Install memcheck tools
 dnf install -y \
