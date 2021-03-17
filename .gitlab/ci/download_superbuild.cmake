92c38e4834a44fb26129703f1c9a72582e02b948
Ben Boeckel <ben.boeckel@kitware.com>
2021-03-17 13:51:06 -0400

    ci: update superbuild image

--- .gitlab/ci/download_superbuild.cmake
+++ .gitlab/ci/download_superbuild.cmake
@@ -8,14 +8,13 @@ cmake_minimum_required(VERSION 3.12)
 set(data_host "https://data.kitware.com")
 
 # Determine the tarball to download. ci-smtk-ci-developer-{date}-{git-sha}.tar.gz
+# 20210420
 if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "vs2019")
-  # 20201124
-  set(file_item "6033ca892fa25629b99c67d8")
-  set(file_hash "55354ae4fbdfb74b1cb09e1535cb855d4bdda8370941ec7f20a6d1f2476fc145bb134d3c6d2eb6dea26168e981051237d880d4fb290a53372405fa5c4e0004f7")
+  set(file_item "607f0ddb2fa25629b9f66898")
+  set(file_hash "c324884ec8f7832b450c948f57e6767735c88fc9a5930675891437b22b6abeb2b40fbb9cb1b498559f648693daaa71b3501e413caceccf88e714d784e9f8e75f")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos")
-  # 20201124
-  set(file_item "6033cb0a2fa25629b99c68b7")
-  set(file_hash "9f3c39e30ca6dce10d61697d73320c08a3c999cbf28449f6b9472ad9e0fa6e4973863917cae9e1a2b201d74d92df05830607efb760455d41160f0c35cca5857a")
+  set(file_item "607f0db12fa25629b9f6684f")
+  set(file_hash "96f7c03cb4da0091b7ded0d680ace228b72b617471d99272d28c4e0bcd42c584a34b700ac81d1ae4a9cc4e0942bf299234a00b4f6193ff8a590b6d8f7713e39a")
 else ()
   message(FATAL_ERROR
     "Unknown build to use for the superbuild")
