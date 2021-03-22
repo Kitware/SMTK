0903ab84c223e83da5361a9d3f604d13e35c1171
Ben Boeckel <ben.boeckel@kitware.com>
2021-03-22 19:21:04 -0400

    ci: use system Qt again
    This reverts commits c84b12e14aa56bb5ef8b2354e26b82fc13d4f942,
    0a7adfbef675e8e200598c2feb5f5279efedb85a, and
    c52a2d757205b54801a327dd97b6d5e2175de477.

--- .gitlab/ci/docker/fedora33-paraview/install_deps.sh
+++ .gitlab/ci/docker/fedora33-paraview/install_deps.sh
@@ -5,18 +5,22 @@ dnf install -y \
     zlib-devel libcurl-devel python-devel python-unversioned-command \
     freeglut-devel glew-devel graphviz-devel libpng-devel mesa-dri-drivers \
     libxcb libxcb-devel libXt-devel xcb-util xcb-util-devel mesa-libGL-devel \
-    libxkbcommon-devel diffutils hostname file openssl-devel
+    libxkbcommon-devel diffutils hostname file
 
 # Install development tools
 dnf install -y \
     gcc-c++ \
+    qt5-qtbase-devel \
+    qt5-qtsvg-devel \
+    qt5-qttools-devel \
+    qt5-qtx11extras-devel \
+    qt5-qtxmlpatterns-devel \
     cmake \
     git-core \
     git-lfs \
     ninja-build \
     make \
-    chrpath \
-    which
+    chrpath
 
 # Install memcheck tools
 dnf install -y \
