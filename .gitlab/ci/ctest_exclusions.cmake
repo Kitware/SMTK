3c44f5e5d7bf40b267bc895eb74093c8a41de846
Ben Boeckel <ben.boeckel@kitware.com>
2021-04-21 10:31:25 -0400

    ci: exclude some test failures on Linux
    VTK lighting seems to be wrong for RenderMesh. OpenExodusFile fails on
    CI, but works locally.

--- .gitlab/ci/ctest_exclusions.cmake
+++ .gitlab/ci/ctest_exclusions.cmake
@@ -1,6 +1,17 @@
 set(test_exclusions
   pv.MeshSelection
 )
+
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "fedora")
+  list(APPEND test_exclusions
+    # VTK lighting seems to be wrong.
+    "^RenderMesh$"
+
+    # Fails in CI; works locally. Needs investigation.
+    "^pv\\.OpenExodusFile$"
+    )
+endif ()
+
 string(REPLACE ";" "|" test_exclusions "${test_exclusions}")
 if (test_exclusions)
   set(test_exclusions "(${test_exclusions})")
