dd314171eaf6d7638317214e5fc8208a4e4140f8
Corey Hart <corey.hart@gm.com>
2021-06-21 15:39:59 -0500

    Check if there is a mainwindow to prevent crash
    Fixes a crash that occurs when SMTK is initialized without creating a main window.  This is mostly applicable when creating unit tests.

--- smtk/extension/paraview/appcomponents/plugin/pqSMTKExportSimulationBehavior.cxx
+++ smtk/extension/paraview/appcomponents/plugin/pqSMTKExportSimulationBehavior.cxx
@@ -223,6 +223,13 @@ pqSMTKExportSimulationBehavior::pqSMTKExportSimulationBehavior(QObject* parent)
 
       QMainWindow* mainWindow = qobject_cast<QMainWindow*>(pqCoreUtilities::mainWidget());
 
+      // Stop here if main window does not exist
+      // This is typically the case for certain unit tests.
+      if (!mainWindow)
+      {
+        return;
+      }
+
       QList<QAction*> menuBarActions = mainWindow->menuBar()->actions();
 
       QMenu* menu = nullptr;
