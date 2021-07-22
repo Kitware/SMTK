e95511ab2860470374ecb0f0af962b8ffbebb3df
Ben Boeckel <ben.boeckel@kitware.com>
2021-07-22 19:11:38 -0400

    CDash: disable submission to the private CDash for now
    It isn't used for anything right now, so avoid causing failing
    dashboards while there's a misconfiguration.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,7 +7,8 @@
 set(CTEST_PROJECT_NAME "SuperBuild-ConceptualModelBuilder")
 set(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
 
-set(drop_sites kitware)
+#set(drop_sites kitware)
+set(drop_sites)
 
 set(CTEST_DROP_METHOD_kitware "https")
 set(CTEST_DROP_SITE_kitware "www.kitware.com/CDash")
