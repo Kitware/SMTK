164a349c3e6ffa5393a61210441323cc5a656506
Ben Boeckel <ben.boeckel@kitware.com>
2021-07-23 16:55:35 -0400

    Deprecation: remove extra parentheses from the reason
    These end up breaking MSVC because it ends up seeing
    `__declspec(deprecated(("reason")))` which is not supported.

--- smtk/common/Deprecation.h
+++ smtk/common/Deprecation.h
@@ -54,7 +54,7 @@
 #endif
 
 #define SMTK_DEPRECATION_REASON(version_major, version_minor, reason)                              \
-  ("SMTK Deprecated in " #version_major "." #version_minor ": " #reason)
+  "SMTK Deprecated in " #version_major "." #version_minor ": " #reason
 
 #if SMTK_DEPRECATION_LEVEL >= SMTK_VERSION_CHECK(21, 7)
 #define SMTK_DEPRECATED_IN_21_07(reason) SMTK_DEPRECATION(SMTK_DEPRECATION_REASON(21, 7, reason))
