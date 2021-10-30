cc2cb6efa729580f95d01b4002418ad44fb076c6
David Thompson <david.thompson@kitware.com>
2021-10-30 13:52:26 -0400

    Provide a pybind11 holder type for scoped raw pointers.
    References and raw pointers that are "scoped" by another object
    python can reference with a shared pointer can be wrapped using
    a custom smart-pointer type.
    
    This is useful for `smtk::task::Manager` since it should
    pass its task and adaptor instance-objects by reference.

--- smtk/task/pybind11/PybindTask.cxx
+++ smtk/task/pybind11/PybindTask.cxx
@@ -30,7 +30,7 @@ using namespace nlohmann;
 
 PYBIND11_DECLARE_HOLDER_TYPE(T, std::shared_ptr<T>);
 
-PYBIND11_MODULE(task, m)
+PYBIND11_MODULE(_smtkPybindTask, m)
 {
   m.doc() = "SMTK tasks are portions of a workflow.";
   py::module smtk = m.def_submodule("smtk", "<description>");
@@ -38,13 +38,13 @@ PYBIND11_MODULE(task, m)
 
   // The order of these function calls is important! It was determined by
   // comparing the dependencies of each of the wrapped objects.
-  auto smtk_task_Active = pybind11_init_smtk_task_Active(task);
-  auto smtk_task_Manager = pybind11_init_smtk_task_Manager(task);
-  auto smtk_task_Registrar = pybind11_init_smtk_task_Registrar(task);
-  auto smtk_task_Task = pybind11_init_smtk_task_Task(task);
-  pybind11_init_smtk_task_State(task);
-  pybind11_init_smtk_task_stateEnum(task);
-  pybind11_init_smtk_task_stateName(task);
-  auto smtk_task_FillOutAttributes = pybind11_init_smtk_task_FillOutAttributes(task);
-  auto smtk_task_GatherResources = pybind11_init_smtk_task_GatherResources(task);
+  auto smtk_task_Active = pybind11_init_smtk_task_Active(m);
+  auto smtk_task_Manager = pybind11_init_smtk_task_Manager(m);
+  auto smtk_task_Registrar = pybind11_init_smtk_task_Registrar(m);
+  auto smtk_task_Task = pybind11_init_smtk_task_Task(m);
+  pybind11_init_smtk_task_State(m);
+  pybind11_init_smtk_task_stateEnum(m);
+  pybind11_init_smtk_task_stateName(m);
+  auto smtk_task_FillOutAttributes = pybind11_init_smtk_task_FillOutAttributes(m);
+  auto smtk_task_GatherResources = pybind11_init_smtk_task_GatherResources(m);
 }
