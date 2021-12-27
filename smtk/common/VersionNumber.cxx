Kitware/SMTK

commit 93bb47168eef7202bf1f2a9b512826a5c4981df9

Add `smtk.attribute_panel.display_hint` property...
... and associated changes to implement it including
+ bumping the XML and JSON version numbers for attribute resources
+ attribute panel changes.
+ do not invoke released observers. This was a bug; released
  observers may reference objects in an improper state and cause
  unwanted side effects.
+ keeping post-processing state in SMTK behavior
  and have the attribute panel synchronize with pipeline sources
  only when in post-processing mode.
+ fixing the display-attribute-on-load behavior because
  the resource-manager observer was always overwriting the attribute
  resource to display – even when it was not suitable to display.

smtk/common/VersionNumber.cxx
@@ -1,0 +1,108 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+#include "smtk/common/VersionNumber.h"
+
+#include <regex>
+#include <sstream>
+
+namespace smtk
+{
+namespace common
+{
+
+/// Default constructor creates a nil VersionNumber (IsNull() == true).
+VersionNumber::VersionNumber()
+  : std::array<int, 3>{ 0, 0, 0 }
+{
+}
+
+/// Copy constructor.
+VersionNumber::VersionNumber(const VersionNumber& other) = default;
+
+/// Construct a VersionNumber from a text string in either major.minor or major.minor.patch format.
+VersionNumber::VersionNumber(const std::string& txt)
+{
+  const std::regex versionRegex("([0-9]+)\\.([0-9]+)\\.?([0-9]+)?");
+  std::smatch versionMatch;
+  if (!txt.empty())
+  {
+    if (std::regex_match(txt, versionMatch, versionRegex))
+    {
+      for (std::size_t ii = 1; ii < versionMatch.size() && ii < 4; ++ii)
+      {
+        auto num = versionMatch[ii].str();
+        // std::cout << "Piece " << ii << " is \"" << num << "\"\n";
+        if (!num.empty())
+        {
+          (*this)[ii - 1] = std::stoul(num);
+        }
+      }
+    }
+  }
+}
+
+int VersionNumber::major() const
+{
+  return (*this)[0];
+}
+
+int VersionNumber::minor() const
+{
+  return (*this)[1];
+}
+
+int VersionNumber::patch() const
+{
+  return (*this)[2];
+}
+
+std::string VersionNumber::string() const
+{
+  std::ostringstream result;
+  result << (*this)[0] << "." << (*this)[1];
+  if ((*this)[2] != 0)
+  {
+    result << "." << (*this)[2];
+  }
+  return result.str();
+}
+
+/// Compare two VersionNumbers for inequality.
+// bool VersionNumber::operator!=(const VersionNumber& other) const = default;
+
+/// Compare two VersionNumbers for equality.
+// bool VersionNumber::operator==(const VersionNumber& other) const = default;
+
+/// Compare two VersionNumbers for ordering.
+// bool VersionNumber::operator<(const VersionNumber& other) const = default;
+
+/// Assignment operator.
+// VersionNumber& VersionNumber::operator=(const VersionNumber& other) = default;
+
+#if 0
+/// Write a VersionNumber to a stream (as a string).
+std::ostream& operator<<(std::ostream& stream, const VersionNumber& uid)
+{
+  stream << uid.toString().c_str();
+  return stream;
+}
+
+/// Read a VersionNumber from a stream (as a string).
+std::istream& operator>>(std::istream& stream, VersionNumber& uid)
+{
+  std::string txt;
+  stream >> txt;
+  uid = VersionNumber(txt);
+  return stream;
+}
+#endif
+
+} // namespace common
+} // namespace smtk

smtk/attribute/json/jsonResource.cxx
@@ -15,6 +15,8 @@
 #include "smtk/attribute/Resource.h"
 #include "smtk/attribute/json/jsonAttribute.h"
 #include "smtk/attribute/json/jsonDefinition.h"
+#include "smtk/common/VersionNumber.h"
+#include "smtk/common/json/jsonVersionNumber.h"
 #include "smtk/io/Logger.h"
 #include "smtk/resource/json/jsonResource.h"
 #include "smtk/view/json/jsonView.h"
@@ -22,6 +24,7 @@
 #include "smtk/CoreExports.h"
 
 #include <queue>
+#include <regex>
 #include <string>
 
 namespace smtk
@@ -35,8 +38,8 @@ using json = nlohmann::json;
 SMTKCORE_EXPORT void to_json(json& j, const smtk::attribute::ResourcePtr& res)
 {
   smtk::resource::to_json(j, smtk::static_pointer_cast<smtk::resource::Resource>(res));
-  // Set the version to 4.0
-  j["version"] = "4.0";
+  // Set the version to 5.0
+  j["version"] = "5.0";
   j["IsPrivate"] = res->isPrivate();
   // Write out the active category information
   if (!res->activeCategories().empty())
@@ -326,6 +329,18 @@ SMTKCORE_EXPORT void from_json(const json& j, smtk::attribute::ResourcePtr& res)
 
   auto resource = std::static_pointer_cast<smtk::resource::Resource>(res);
   smtk::resource::from_json(j, resource);
+  auto jversion = j.find("version");
+  smtk::common::VersionNumber version;
+  if (jversion != j.end())
+  {
+    version = jversion->get<smtk::common::VersionNumber>();
+  }
+
+  // Starting with version 5, this property must be explicitly present in the file.
+  if (version.major() < 5)
+  {
+    resource->properties().get<bool>()["smtk.attribute_panel.display_hint"] = true;
+  }
 
   // Set Private State
   auto isPrivateResult = j.find("IsPrivate");

smtk/common/CMakeLists.txt
@@ -10,6 +10,7 @@ set(commonSrcs
   InfixExpressionGrammar.cxx
   json/jsonLinks.cxx
   json/jsonUUID.cxx
+  json/jsonVersionNumber.cxx
   Managers.cxx
   Paths.cxx
   StringUtil.cxx
@@ -18,6 +19,7 @@ set(commonSrcs
   TypeContainer.cxx
   UUID.cxx
   UUIDGenerator.cxx
+  VersionNumber.cxx
 )
 
 set(commonHeaders
@@ -41,6 +43,7 @@ set(commonHeaders
   json/jsonLinks.h
   json/jsonTypeMap.h
   json/jsonUUID.h
+  json/jsonVersionNumber.h
   Links.h
   Managers.h
   Observers.h
@@ -57,6 +60,7 @@ set(commonHeaders
   TypeContainer.h
   UUID.h
   UUIDGenerator.h
+  VersionNumber.h
   VersionMacros.h
   Visit.h
   testing/cxx/helpers.h

smtk/common/Observers.h
@@ -237,7 +237,10 @@ public:
           std::cerr << "Calling observer (" << entry.first.first << ", " << entry.first.second
                     << "): " << m_descriptions[entry.first] << std::endl;
         }
-        result |= entry.second(std::forward<Types>(args)...);
+        if (entry.first.assigned())
+        {
+          result |= entry.second(std::forward<Types>(args)...);
+        }
       }
       else if (DebugObservers)
       {
@@ -286,7 +289,10 @@ public:
           std::cerr << "Calling observer (" << entry.first.first << ", " << entry.first.second
                     << "): " << m_descriptions[entry.first] << std::endl;
         }
-        entry.second(std::forward<Types>(args)...);
+        if (entry.first.assigned())
+        {
+          entry.second(std::forward<Types>(args)...);
+        }
       }
       else if (DebugObservers)
       {

smtk/common/UUID.h
@@ -7,8 +7,8 @@
 //  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 //  PURPOSE.  See the above copyright notice for more information.
 //=========================================================================
-#ifndef __smtk_common_UUID_h
-#define __smtk_common_UUID_h
+#ifndef smtk_common_UUID_h
+#define smtk_common_UUID_h
 
 #include "smtk/CoreExports.h"
 #include "smtk/HashFunctor.h"
@@ -144,4 +144,4 @@ inline size_t hash<smtk::common::UUID>::operator()(smtk::common::UUID uid) const
 
 SMTK_HASH_NS_END
 
-#endif // __smtk_common_UUID_h
+#endif // smtk_common_UUID_h

smtk/common/VersionNumber.h
@@ -1,0 +1,62 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+#ifndef smtk_common_VersionNumber_h
+#define smtk_common_VersionNumber_h
+
+#include "smtk/CoreExports.h"
+#include "smtk/SystemConfig.h"
+
+#include <array>
+#include <string>
+
+// On some systems, major and minor are defined as macros. If this is one of
+// those systems, undefine these macros before defining the methods
+// of smtk::common::VersionNumber.
+#ifdef major
+#undef major
+#endif
+
+#ifdef minor
+#undef minor
+#endif
+
+namespace smtk
+{
+namespace common
+{
+
+/**\brief A 2- or 3-component version number (i.e., major, minor, and optional patch).
+  *
+  */
+class SMTKCORE_EXPORT VersionNumber : public std::array<int, 3>
+{
+public:
+  VersionNumber();
+  VersionNumber(const VersionNumber& other);
+  VersionNumber(int major, int minor, int patch = 0);
+  VersionNumber(const std::string& versionString);
+
+  int major() const;
+  int minor() const;
+  int patch() const;
+
+  std::string string() const;
+
+  // bool operator==(VersionNumber const& other) const = default;
+  // bool operator<(VersionNumber const& other) const = default;
+};
+
+// SMTKCORE_EXPORT std::ostream& operator<<(std::ostream& stream, const VersionNumber& uid);
+// SMTKCORE_EXPORT std::istream& operator>>(std::istream& stream, VersionNumber& uid);
+
+} // namespace common
+} // namespace smtk
+
+#endif // smtk_common_VersionNumber_h

smtk/common/json/jsonVersionNumber.cxx
@@ -1,0 +1,33 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+#include "jsonVersionNumber.h"
+
+#include <regex>
+#include <string>
+
+using json = nlohmann::json;
+// Override how VersionNumbers are serialized.
+// Without this, they appear as JSON arrays of unsigned integers.
+namespace smtk
+{
+namespace common
+{
+SMTKCORE_EXPORT void to_json(json& j, const smtk::common::VersionNumber& v)
+{
+  j = v.string();
+}
+
+SMTKCORE_EXPORT void from_json(const json& j, smtk::common::VersionNumber& v)
+{
+  v = VersionNumber(j.get<std::string>());
+}
+
+} // namespace common
+} // namespace smtk

smtk/common/json/jsonVersionNumber.h
@@ -1,0 +1,30 @@
+//=========================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//=========================================================================
+#ifndef smtk_common_json_jsonVersionNumber_h
+#define smtk_common_json_jsonVersionNumber_h
+
+#include "smtk/common/VersionNumber.h"
+
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+// Override how VersionNumber numbers are serialized.
+// Without this, they appear as JSON arrays of unsigned integers.
+namespace smtk
+{
+namespace common
+{
+SMTKCORE_EXPORT void to_json(json& j, const smtk::common::VersionNumber& opt);
+
+SMTKCORE_EXPORT void from_json(const json& j, smtk::common::VersionNumber& opt);
+} // namespace common
+} // namespace smtk
+
+#endif

commit cffaea2c37f09363147cfd0b131695cc974f95fd

COMP: Update VersionNumber.cxx to fix build against C++11 prior CWG 1270
This commit fixes error like the following introduced in 93bb4716 and
reported when building SMTK on system providing C++11 prior to the CWG
1270 revision:

  /path/to/smtk/smtk/common/VersionNumber.cxx:22:34: error: array must be initialized with a brace-enclosed initializer
     : std::array<int, 3>{ -1, 0, 0 }
                                    ^

See https://en.cppreference.com/w/cpp/container/array#Example

Fixes #455

smtk/common/VersionNumber.cxx
@@ -19,12 +19,12 @@ namespace common
 
 /// Default constructor creates a nil VersionNumber (IsNull() == true).
 VersionNumber::VersionNumber()
-  : std::array<int, 3>{ -1, 0, 0 }
+  : std::array<int, 3>{ { -1, 0, 0 } }
 {
 }
 
 VersionNumber::VersionNumber(int major, int minor, int patch)
-  : std::array<int, 3>{ major, minor, patch }
+  : std::array<int, 3>{ { major, minor, patch } }
 {
 }
 