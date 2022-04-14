0f4d223dc4f54b4235cb22bb254ecf21c993760d
Ryan Krattiger <ryan.krattiger@kitware.com>
2022-04-14 09:29:42 -0500

    CI: Fix download file ids

--- .gitlab/ci/download_superbuild.cmake
+++ .gitlab/ci/download_superbuild.cmake
@@ -10,13 +10,13 @@ set(data_host "https://data.kitware.com")
 # Determine the tarball to download. ci-smtk-ci-developer-{date}-{git-sha}.tar.gz
 # 20220321: Bump to catch pybind11 superbuild change and occt for contract test.
 if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "vs2019")
-  set(file_item "6251894d4acac99f42e862b5")
+  set(file_item "625251884acac99f42ea97d9")
   set(file_hash "b10ba9d365a611c5b73be6fc6f70b3df337ed33763fea73305649eae48ba32cd3b7affd56bf70f26ca4b74b862a6bc589eae7376d2655074af06f56a3186ca96")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos_x86_64")
-  set(file_item "62511c184acac99f42e60442")
+  set(file_item "625251884acac99f42ea97d5")
   set(file_hash "a86cb87bfdb53bb719267f79338b54e60835c1c9044f455f8dddda7dfde2e12ea9c1f8ef57f50ff598465e0308e52b82906780300df6d53fe7286b0a07eb6ad2")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos_arm64")
-  set(file_item "625119db4acac99f42e5a3d7")
+  set(file_item "625251884acac99f42ea97d1")
   set(file_hash "2ed1ebe3c4a44f660cdb3ff91dac4d9bf94981238ed4cbcb20ad41e4032fc2dfc4a0f283680875f025f887323c6f5b82b6ddf96c81ebc1471c34ae8fbca05f38")
 else ()
   message(FATAL_ERROR
