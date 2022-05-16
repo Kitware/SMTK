cd853fb0ca32b20e7a0fa3a5938670c6dba4dd8b
John Tourtellott <john.tourtellott@kitware.com>
2022-05-16 13:35:29 -0400

    Add contract test for cmb-2d (linux)

--- .gitlab/ci/configure_fedora33_paraview.cmake
+++ .gitlab/ci/configure_fedora33_paraview.cmake
@@ -6,6 +6,7 @@ set(SMTK_PLUGIN_CONTRACT_FILE_URLS
   "https://gitlab.kitware.com/cmb/cmb/-/raw/master/cmake/cmb.cmake"
   "https://gitlab.kitware.com/cmb/plugins/ace3p-extensions/-/raw/master/CMake/ace3p-extensions.cmake"
   "https://gitlab.kitware.com/cmb/plugins/xmsmeshoperation/-/raw/master/CMake/xms-mesh-operation.cmake"
+  "https://gitlab.kitware.com/cmb/plugins/cmb-2d/-/raw/master/cmake/cmb-2d.cmake"
   CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora33.cmake")
