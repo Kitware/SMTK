e1e871ec1b308eabb52b527e5e3dd1cd7ae73425
Ben Boeckel <ben.boeckel@kitware.com>
2022-07-04 07:01:31 -0400

    cmb/cmb: remove CMB contract tests
    Incompatibilities have been made as far as testing XML is concerned.

--- .gitlab/ci/configure_fedora33_paraview.cmake
+++ .gitlab/ci/configure_fedora33_paraview.cmake
@@ -3,7 +3,6 @@ set(SMTK_PLUGIN_CONTRACT_FILE_URLS
   "https://gitlab.kitware.com/cmb/plugins/adh-extensions/-/raw/master/CMake/adh-extensions.cmake"
   "https://gitlab.kitware.com/cmb/plugins/rgg-session/-/raw/master/CMake/rgg-session.cmake"
   "https://gitlab.kitware.com/cmb/plugins/truchas-extensions/-/raw/master/CMake/truchas-extensions.cmake"
-  "https://gitlab.kitware.com/cmb/cmb/-/raw/master/cmake/cmb.cmake"
   "https://gitlab.kitware.com/cmb/plugins/ace3p-extensions/-/raw/master/CMake/ace3p-extensions.cmake"
   "https://gitlab.kitware.com/cmb/plugins/xmsmeshoperation/-/raw/master/CMake/xms-mesh-operation.cmake"
   "https://gitlab.kitware.com/cmb/plugins/cmb-2d/-/raw/master/cmake/cmb-2d.cmake"
