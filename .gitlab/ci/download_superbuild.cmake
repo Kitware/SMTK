df0b261ffac9f3b095fcec5404971a559fb2769b
David Thompson <david.thompson@kitware.com>
2022-08-19 05:41:03 -0400

    Bump CI images to catch eigen 3.4.0.

--- .gitlab/ci/download_superbuild.cmake
+++ .gitlab/ci/download_superbuild.cmake
@@ -8,16 +8,16 @@ cmake_minimum_required(VERSION 3.12)
 set(data_host "https://data.kitware.com")
 
 # Determine the tarball to download. ci-smtk-ci-developer-{date}-{git-sha}.tar.gz
-# 20220321: Bump to catch pybind11 superbuild change and occt for contract test.
+# 20220818: Bump to catch eigen 3.4.0 from cmb-sb.
 if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "vs2019")
-  set(file_item "625251884acac99f42ea97d9")
-  set(file_hash "b10ba9d365a611c5b73be6fc6f70b3df337ed33763fea73305649eae48ba32cd3b7affd56bf70f26ca4b74b862a6bc589eae7376d2655074af06f56a3186ca96")
+  set(file_item "62fec7c0f64de9b950f36a45")
+  set(file_hash "8554be34f3a547701ed4aa4ef7f872980de6b4d7216e22fc13470285112bfcabcf524b51e1752f7161a8dee87775df2f389f31aa75c4e5495cb58020e2e2eaa2")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos_x86_64")
-  set(file_item "625251884acac99f42ea97d5")
-  set(file_hash "a86cb87bfdb53bb719267f79338b54e60835c1c9044f455f8dddda7dfde2e12ea9c1f8ef57f50ff598465e0308e52b82906780300df6d53fe7286b0a07eb6ad2")
+  set(file_item "62fec7c0f64de9b950f36a41")
+  set(file_hash "c96985f574688323ffca7fc7a13e439879f961bf79962d687abcabae95c83261b89a2f4d0f9afe27e71ac97bad14f91160bf2f379127641a3d889bca0a13ac5e")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos_arm64")
-  set(file_item "625251884acac99f42ea97d1")
-  set(file_hash "2ed1ebe3c4a44f660cdb3ff91dac4d9bf94981238ed4cbcb20ad41e4032fc2dfc4a0f283680875f025f887323c6f5b82b6ddf96c81ebc1471c34ae8fbca05f38")
+  set(file_item "62fec7c0f64de9b950f36a31")
+  set(file_hash "138165f3f56a290a721f54456fd1e8ac048ca546048384532e3d63e0476cccff976b73409e701b74ed4fe80a1d83064c763b38337382296267fb83f733f29d33")
 else ()
   message(FATAL_ERROR
     "Unknown build to use for the superbuild")
