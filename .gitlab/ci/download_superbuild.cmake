0877ed1b05492cbbdeb4d58ede2349f0f72be6b0
Aron Helser <aron.helser@kitware.com>
2023-06-28 12:55:57 -0400

    Bump ci images for units lib fix, vtk-only image available.

--- .gitlab/ci/download_superbuild.cmake
+++ .gitlab/ci/download_superbuild.cmake
@@ -1,5 +1,7 @@
 cmake_minimum_required(VERSION 3.12)
 
+# tarballs are uploaded by CI to the "ComputationalModelBuilder" group,
+# "ci" folder, then a dated subfolder.
 # In order to use a new superbuild, please copy the item from the date stamped
 # directory into the `keep` directory. This ensure that new versions will not
 # be removed by the uploader script and preserve them for debugging in the
@@ -8,16 +10,16 @@ cmake_minimum_required(VERSION 3.12)
 set(data_host "https://data.kitware.com")
 
 # Determine the tarball to download. ci-smtk-ci-developer-{date}-{git-sha}.tar.gz
-# 20230626: Bump to catch units dependency.
+# 20230628: Bump to catch units dependency update.
 if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "vs2022")
-  set(file_item "6499bf6e5121da8eb1a8124f")
-  set(file_hash "bafc715b93bf50340cf7345ccb68e04fd2edc415bea9ddd4df30f3f749488ade4144557d39c5939bc3442c04fe3c8f919b54f11888812e5ea1d35e0aee768b24")
+  set(file_item "649c62e45121da8eb1a813c2")
+  set(file_hash "0685430e8ac29263e9f2d53d89eeaf60fc5e69a2c1f246665cc4aec1485ea7f42dbf1bd21dfd85809bdf4be3a8bfde2ffa1c7a9a82b4ab9cfca72381ba423f80")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos_x86_64")
-  set(file_item "6499bf6e5121da8eb1a8124d")
-  set(file_hash "5dc484ea42496f98398fa90ab16c1370348f139836ac02bf82636aad0c22fefc0a106ef05c1e28daa59c4dbffc8261d1bbb5f33187dbeb6be33d94cd514c0a6d")
+  set(file_item "649c62e45121da8eb1a813c0")
+  set(file_hash "810441065683878b8f30666b4a3d294c57528494fd5a1944983f8c110eb39ec489dae31e3b3969b7864caa591cc7be7d092150c1bdfce5d83885f66b11c34eb5")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos_arm64")
-  set(file_item "6499bf6e5121da8eb1a8124b")
-  set(file_hash "1c4e2b54c4ae0b36807b974c1200ffa25593a1cb462adc75746c23f7bdc42450fc201c99d6083d034876540faf11af8cc0002d7795c8565f0bd30ea1cbad74b4")
+  set(file_item "649c62e45121da8eb1a813be")
+  set(file_hash "bec92256f6ecdee7befb40bc1ee531ad0136cc5971d72e1a60b6fabb92ae8b22b1305c28f71937b3016e96f80e2221e9263c0a0a8a4386b8b411754a0c21180f")
 else ()
   message(FATAL_ERROR
     "Unknown build to use for the superbuild")
