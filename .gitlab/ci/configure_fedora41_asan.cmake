21f63217707ac4ce39bdede30309ff9aeac0464a
Ben Boeckel <ben.boeckel@kitware.com>
2025-01-06 02:35:55 +0100

    gitlab-ci: update to use Fedora 41 images

--- .gitlab/ci/configure_fedora41_asan.cmake
+++ .gitlab/ci/configure_fedora41_asan.cmake
@@ -1,4 +1,4 @@
 set(SMTK_ENABLE_SANITIZER ON CACHE BOOL "")
 set(SMTK_SANITIZER "address" CACHE STRING "")
 
-include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora33_paraview.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora41_paraview.cmake")
