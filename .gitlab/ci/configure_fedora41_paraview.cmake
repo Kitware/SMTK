8971895d9fc1f0f14fd0221318d221036b980012
David Thompson <david.thompson@kitware.com>
2025-03-16 00:17:05 -0400

    Eliminate contract tests requiring `smtk::mesh`.

--- .gitlab/ci/configure_fedora41_paraview.cmake
+++ .gitlab/ci/configure_fedora41_paraview.cmake
@@ -1,15 +1,12 @@
 set(SMTK_PLUGIN_CONTRACT_FILE_URLS
-  "https://gitlab.kitware.com/cmb/plugins/openfoam-windtunnel/-/raw/master/cmake/openfoam-windtunnel.cmake"
+  # Disabled until importing into smtk::mesh can be replaced with another resource type.
+  # "https://gitlab.kitware.com/cmb/plugins/openfoam-windtunnel/-/raw/master/cmake/openfoam-windtunnel.cmake"
   "https://gitlab.kitware.com/cmb/plugins/opencascade-session/-/raw/master/cmake/opencascade-smtk-contract.cmake"
-  "https://gitlab.kitware.com/cmb/plugins/adh-extensions/-/raw/master/CMake/adh-extensions.cmake"
-  "https://gitlab.kitware.com/cmb/plugins/rgg-session/-/raw/master/CMake/rgg-session.cmake"
   # https://gitlab.kitware.com/cmb/smtk/-/issues/543
   # "https://gitlab.kitware.com/cmb/plugins/truchas-extensions/-/raw/master/CMake/truchas-extensions.cmake"
   "https://gitlab.kitware.com/cmb/cmb/-/raw/master/cmake/cmb.cmake"
   # https://gitlab.kitware.com/cmb/smtk/-/issues/543
   # "https://gitlab.kitware.com/cmb/plugins/ace3p-extensions/-/raw/master/CMake/ace3p-extensions.cmake"
-  "https://gitlab.kitware.com/cmb/plugins/xmsmeshoperation/-/raw/master/CMake/xms-mesh-operation.cmake"
-  "https://gitlab.kitware.com/cmb/plugins/cmb-2d/-/raw/master/cmake/cmb-2d.cmake"
   CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora41.cmake")
